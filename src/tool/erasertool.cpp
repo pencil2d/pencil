fa36582ad9c96eb037ac9933b5aa45013522d422
chchwy <chchwy+bitbucket@gmail.com>
2013-05-21 14:59:07 +0800

    minor, reorder statements

--- src/tool/erasertool.cpp
+++ src/tool/erasertool.cpp
@@ -16,15 +16,15 @@ ToolType EraserTool::type()
 void EraserTool::loadSettings()
 {
     QSettings settings("pencil", "pencil");
-    properties.width = settings.value("eraserWidth").toDouble();
+    properties.width = settings.value("eraserWidth").toDouble();    
+    properties.feather = 0;
+    properties.pressure = 1;
+    properties.invisibility = -1;
+    properties.preserveAlpha = 0;
+
     if (properties.width == 0)
     {
         properties.width = 24;
         settings.setValue("eraserWidth", properties.width);
     }
-    properties.feather = 0;
-    properties.opacity = 0.5;
-    properties.pressure = 1;
-    properties.invisibility = -1;
-    properties.preserveAlpha = 0;
 }
