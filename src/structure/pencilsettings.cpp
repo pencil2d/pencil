47968a3447dbb15355a4dad51f1d648728e43aaa
chchwy <chchwy@gmail.com>
2013-05-23 00:59:34 +0800

    pencilSettings: check the the fresh state of settings

--- src/structure/pencilsettings.cpp
+++ src/structure/pencilsettings.cpp
@@ -4,11 +4,20 @@
 // ==== Singleton ====
 static QSettings* g_pSettings = NULL;
 
+void checkSettings(QSettings* pSettings)
+{
+    if ( !pSettings->contains(kSettingToolCursor) )
+    {
+        pSettings->setValue( kSettingToolCursor, true );
+    }
+}
+
 QSettings* pencilSettings()
 {
     if ( g_pSettings == NULL )
     {
         g_pSettings = new QSettings("pencil", "pencil");
+        checkSettings(g_pSettings);
     }
     return g_pSettings;
 }
