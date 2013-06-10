a773ea99602229fe5fb915336e66433b9319ea31
chchwy <chchwy@gmail.com>
2013-06-11 01:23:07 +0800

    KeyboardManager: minor

--- src/util/keyboardmanager.cpp
+++ src/util/keyboardmanager.cpp
@@ -1,6 +1,21 @@
+
+#include <QSettings>
 #include "keyboardmanager.h"
+
+
+#define SHORTCUTS_INI "shortcuts.ini"
+
 
 KeyboardManager::KeyboardManager(QObject *parent) :
     QObject(parent)
 {
+    m_pKeySettings = new QSettings(SHORTCUTS_INI, QSettings::IniFormat);
+
+
+}
+
+
+void KeyboardManager::setShortcut(ACTION eAction, QString strKeys)
+{
+
 }
--- src/util/keyboardmanager.h
+++ src/util/keyboardmanager.h
@@ -3,16 +3,30 @@
 
 #include <QObject>
 
+
+class QSettings;
+
+
+enum ACTION
+{
+    UNKOWN_ACTION = 0,
+    OPEN_FILE_ATION
+};
+
+
 class KeyboardManager : public QObject
 {
     Q_OBJECT
 public:
     explicit KeyboardManager(QObject *parent = 0);
-    
+    void setShortcut(ACTION eAction, QString strKeys);
+    QString getShortcut(ACTION eAction);
 signals:
     
 public slots:
     
+private:
+    QSettings* m_pKeySettings;
 };
 
 #endif // KEYBOARDMANAGER_H
