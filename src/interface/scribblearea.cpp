efd38dc04d2184e294fda3dd48e9f8f23fb2ae94
Manuel Odendahl <wesen@ruinwesen.com>
2013-07-27 14:37:28 +0200

    add a sanity check when drawing curves for move

--- src/interface/scribblearea.cpp
+++ src/interface/scribblearea.cpp
@@ -1215,11 +1215,21 @@ void ScribbleArea::paintEvent(QPaintEvent *event)
                 bufferImg->clear();
                 QPen pen2(Qt::black, 0.5, Qt::SolidLine, Qt::RoundCap, Qt::RoundJoin);
                 QColor colour = QColor(100, 100, 255);
+
                 for (int k = 0; k < closestCurves.size(); k++)
                 {
                     qreal scale = myTempView.det();
+                    int idx = closestCurves[k];
+                    if (vectorImage->curve.size() <= idx)
+                    {
+                        // safety check
+                        continue;
+                    }
                     BezierCurve myCurve = vectorImage->curve[closestCurves[k]];
-                    if (myCurve.isPartlySelected()) { myCurve.transform(selectionTransformation); }
+                    if (myCurve.isPartlySelected())
+                    {
+                        myCurve.transform(selectionTransformation);
+                    }
                     QPainterPath path = myCurve.getStrokedPath(1.2 / scale, false);
                     bufferImg->drawPath((myView * transMatrix * centralView).map(path), pen2, colour, QPainter::CompositionMode_SourceOver, m_antialiasing);
                 }
