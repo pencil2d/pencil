a720a244f8a874fc59476b4a0da71089a85116a5
chchwy <chchwy@gmail.com>
2013-08-11 17:19:41 +0800

    Enable Tablet high resolution

--- src/interface/scribblearea.cpp
+++ src/interface/scribblearea.cpp
@@ -697,6 +697,7 @@ void ScribbleArea::tabletEvent(QTabletEvent *event)
     //qDebug() << "Device" << event->device() << "Pointer type" << event->pointerType();
     m_strokeManager->tabletEvent(event);
 
+    //qDebug() << event->hiResGlobalPos();
     currentTool()->adjustPressureSensitiveProperties(m_strokeManager->getPressure(),
         event->pointerType() == QTabletEvent::Cursor);
 
