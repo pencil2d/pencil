def896e0a088e3548fdf7e4a0da744b173d810df
Cirus <evolutivemedia@gmail.com>
2013-10-22 13:12:43 +0200

    Better way to select start layer (in all cases)

--- src/interface/mainwindow2.cpp
+++ src/interface/mainwindow2.cpp
@@ -83,8 +83,6 @@ MainWindow2::MainWindow2(QWidget *parent) :
     connect(m_toolSet, SIGNAL(clearButtonClicked()), editor, SLOT(clearCurrentFrame()));
     connect(editor, SIGNAL(changeTool(ToolType)), m_toolSet, SLOT(setCurrentTool(ToolType)));        
 
-    editor->setCurrentLayer( this->editor->object->getLayerCount()-1 );
-
 }
 
 MainWindow2::~MainWindow2()
