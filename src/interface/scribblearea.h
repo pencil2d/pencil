022649d418e0687978e9c2baae0f9576c62f7c73
Matt Chang <chchwy@gmail.com>
2013-11-16 23:00:45 +0800

    Code formatting

--- src/interface/scribblearea.h
+++ src/interface/scribblearea.h
@@ -45,14 +45,14 @@ class ScribbleArea : public QWidget
     friend class SmudgeTool;
 
 public:
-    ScribbleArea(QWidget *parent = 0, Editor *m_pEditor = 0);
+    ScribbleArea( QWidget *parent = 0, Editor *m_pEditor = 0 );
 
-    void next(const int &i);
+    void next( const int &i );
 
     void resetTools();
 
     void deleteSelection();
-    void setSelection(QRectF rect, bool);
+    void setSelection( QRectF rect, bool );
     void displaySelectionProperties();
     QRectF getSelection() const { return mySelection; }
     bool somethingSelected;
@@ -64,7 +64,7 @@ public:
     bool areLayersSane() const;
     bool isLayerPaintable() const;
 
-    static QBrush getBackgroundBrush(QString);
+    static QBrush getBackgroundBrush( QString );
 
     bool showThinLines() const { return m_showThinLines; }
     int showAllLayers() const { return m_showAllLayers; }
@@ -73,9 +73,9 @@ public:
     bool usePressure() const { return m_usePressure; }
     bool makeInvisible() const { return m_makeInvisible; }
 
-    enum MoveMode { MIDDLE, TOPLEFT, TOPRIGHT, BOTTOMLEFT, BOTTOMRIGHT, ROTATION};
+    enum MoveMode { MIDDLE, TOPLEFT, TOPRIGHT, BOTTOMLEFT, BOTTOMRIGHT, ROTATION };
     MoveMode getMoveMode() const { return m_moveMode; }
-    void setMoveMode(MoveMode moveMode) { m_moveMode = moveMode; }
+    void setMoveMode( MoveMode moveMode ) { m_moveMode = moveMode; }
 
     QMatrix getView();
     QRectF getViewRect();
@@ -85,49 +85,49 @@ public:
     qreal getTempViewScaleX() const { return myTempView.m11(); }
     qreal getViewScaleY() const { return myView.m22(); }
     qreal getTempViewScaleY() const { return myTempView.m22(); }
-    qreal getCentralViewScale() const { return ( sqrt(centralView.det()) ) ; }
+    qreal getCentralViewScale() const { return (sqrt( centralView.det() )); }
 
     QMatrix getTransformationMatrix() const { return transMatrix; }
-    void setTransformationMatrix(QMatrix matrix);
+    void setTransformationMatrix( QMatrix matrix );
     void applyTransformationMatrix();
 
     void updateFrame();
-    void updateFrame(int frame);
+    void updateFrame( int frame );
     void updateAllFrames();
     void updateAllVectorLayersAtCurrentFrame();
-    void updateAllVectorLayersAt(int frame);
+    void updateAllVectorLayersAt( int frame );
     void updateAllVectorLayers();
 
-    bool shouldUpdateAll() const {  return updateAll; }
+    bool shouldUpdateAll() const { return updateAll; }
     void setAllDirty() { updateAll = true; }
 
     BaseTool *currentTool();
-    BaseTool *getTool(ToolType eToolMode);
-    void setCurrentTool(ToolType eToolMode);
-    void setTemporaryTool(ToolType eToolMode);
-    void switchTool(ToolType type);
+    BaseTool *getTool( ToolType eToolMode );
+    void setCurrentTool( ToolType eToolMode );
+    void setTemporaryTool( ToolType eToolMode );
+    void switchTool( ToolType type );
 
     void setPrevTool();
 
-    QPointF pixelToPoint(QPointF pixel);
+    QPointF pixelToPoint( QPointF pixel );
 
     StrokeManager *getStrokeManager() const { return m_strokeManager; }
 
     PopupColorPaletteWidget *getPopupPalette() const { return m_popupPaletteWidget; }
 
-    void keyPressed(QKeyEvent *event);
+    void keyPressed( QKeyEvent *event );
 
     Editor *getEditor() { return m_pEditor; }
 
 signals:
     void modification();
-    void modification(int);
-    void thinLinesChanged(bool);
-    void outlinesChanged(bool);
+    void modification( int );
+    void thinLinesChanged( bool );
+    void outlinesChanged( bool );
     //void showAllLayersChanged(bool);
 
-    void onionPrevChanged(bool);
-    void onionNextChanged(bool);
+    void onionPrevChanged( bool );
+    void onionNextChanged( bool );
 
     void pencilOn();
     void eraserOn();
@@ -141,52 +141,52 @@ signals:
     void brushOn();
     void smudgeOn();
 
-public slots:
+    public slots:
     void clearImage();
     void calculateSelectionRect();
     void calculateSelectionTransformation();
     void paintTransformedSelection();
-    void setModified(int layerNumber, int frameNumber);
+    void setModified( int layerNumber, int frameNumber );
 
     void selectAll();
     void deselectAll();
 
-    void toggleOnionPrev(bool);
-    void toggleOnionNext(bool);
-    void toggleOnionBlue(bool);
-    void toggleOnionRed(bool);
-    void toggleGridA(bool);
-    void toggleGridB(bool);
+    void toggleOnionPrev( bool );
+    void toggleOnionNext( bool );
+    void toggleOnionBlue( bool );
+    void toggleOnionRed( bool );
+    void toggleGridA( bool );
+    void toggleGridB( bool );
     void grid();
     //void pressureSlot(int);
     //void invisibleSlot(int);
 
     void resetView();
-    void setMyView(QMatrix view);
+    void setMyView( QMatrix view );
     QMatrix getMyView();
 
     void zoom();
     void zoom1();
     void rotatecw();
     void rotateacw();
-    void setWidth(const qreal);
-    void setFeather(const qreal);
-    void setOpacity(const qreal);
-    void setPressure(const bool);
-    void setInvisibility(const bool);
-    void setPreserveAlpha(const bool);
-    void setFollowContour(const bool);
+    void setWidth( const qreal );
+    void setFeather( const qreal );
+    void setOpacity( const qreal );
+    void setPressure( const bool );
+    void setInvisibility( const bool );
+    void setPreserveAlpha( const bool );
+    void setFollowContour( const bool );
 
-    void setCurveOpacity(int);
-    void setCurveSmoothing(int);
-    void setHighResPosition(int);
-    void setAntialiasing(int);
-    void setGradients(int);
-    void setBackground(int);
-    void setBackgroundBrush(QString);
-    void setShadows(int);
-    void setToolCursors(int);
-    void setStyle(int);
+    void setCurveOpacity( int );
+    void setCurveSmoothing( int );
+    void setHighResPosition( int );
+    void setAntialiasing( int );
+    void setGradients( int );
+    void setBackground( int );
+    void setBackgroundBrush( QString );
+    void setShadows( int );
+    void setToolCursors( int );
+    void setStyle( int );
     void toggleThinLines();
     void toggleOutlines();
     void toggleMirror();
@@ -197,43 +197,43 @@ public slots:
     void togglePopupPalette();
 
 protected:
-    void tabletEvent(QTabletEvent *event);
-    void wheelEvent(QWheelEvent *event);
-    void mousePressEvent(QMouseEvent *event);
-    void mouseMoveEvent(QMouseEvent *event);
-    void mouseReleaseEvent(QMouseEvent *event);
-    void mouseDoubleClickEvent(QMouseEvent *event);
-    void keyPressEvent(QKeyEvent *event);
-    void keyReleaseEvent(QKeyEvent *event);
-    void paintEvent(QPaintEvent *event);
-    void resizeEvent(QResizeEvent *event);
+    void tabletEvent( QTabletEvent *event );
+    void wheelEvent( QWheelEvent *event );
+    void mousePressEvent( QMouseEvent *event );
+    void mouseMoveEvent( QMouseEvent *event );
+    void mouseReleaseEvent( QMouseEvent *event );
+    void mouseDoubleClickEvent( QMouseEvent *event );
+    void keyPressEvent( QKeyEvent *event );
+    void keyReleaseEvent( QKeyEvent *event );
+    void paintEvent( QPaintEvent *event );
+    void resizeEvent( QResizeEvent *event );
     void toggledOnionColor();
     void recentre();
     void setView();
-    void setView(QMatrix);
+    void setView( QMatrix );
 
 public:
-    void drawPolyline(QList<QPointF> points, QPointF lastPoint);
-    void endPolyline(QList<QPointF> points);
+    void drawPolyline( QList<QPointF> points, QPointF lastPoint );
+    void endPolyline( QList<QPointF> points );
 
-    void drawLine( QPointF P1, QPointF P2, QPen pen, QPainter::CompositionMode cm);
-    void drawPath(QPainterPath path, QPen pen, QBrush brush, QPainter::CompositionMode cm);
-    void drawBrush(QPointF thePoint, qreal brushWidth, qreal offset, QColor fillColour, qreal opacity);
+    void drawLine( QPointF P1, QPointF P2, QPen pen, QPainter::CompositionMode cm );
+    void drawPath( QPainterPath path, QPen pen, QBrush brush, QPainter::CompositionMode cm );
+    void drawBrush( QPointF thePoint, qreal brushWidth, qreal offset, QColor fillColour, qreal opacity );
     void drawTexturedBrush( BitmapImage *bmiSource_, QPointF srcPoint_, QPointF thePoint_, qreal brushWidth_, qreal offset_, qreal opacity_ );
     void blurBrush( BitmapImage *bmiSource_, QPointF srcPoint_, QPointF thePoint_, qreal brushWidth_, qreal offset_, qreal opacity_ );
     void liquifyBrush( BitmapImage *bmiSource_, QPointF srcPoint_, QPointF thePoint_, qreal brushWidth_, qreal offset_, qreal opacity_ );
-    void floodFill(VectorImage *vectorImage, QPoint point, QRgb targetColour, QRgb replacementColour, int tolerance);
+    void floodFill( VectorImage *vectorImage, QPoint point, QRgb targetColour, QRgb replacementColour, int tolerance );
 
     void paintBitmapBuffer();
     void clearBitmapBuffer();
-    void refreshBitmap(QRect rect, int rad);
-    void refreshVector(QRect rect, int rad);
-    void setGaussianGradient(QGradient &gradient, QColor colour, qreal opacity, qreal offset);
+    void refreshBitmap( QRect rect, int rad );
+    void refreshVector( QRect rect, int rad );
+    void setGaussianGradient( QGradient &gradient, QColor colour, qreal opacity, qreal offset );
 
 protected:
-    void updateCanvas(int frame, QRect rect);
+    void updateCanvas( int frame, QRect rect );
 
-    void floodFillError(int errorType);
+    void floodFillError( int errorType );
 
     MoveMode m_moveMode;
     ToolType prevMode;
@@ -298,5 +298,3 @@ protected:
 };
 
 #endif
-
-
