91541c6df3c45658462dcda82354cb0a7e6edd48
chchwy <chchwy@gmail.com>
2014-07-20 17:56:23 +0800

    Simplify start flow.

--- app/main.cpp
+++ app/main.cpp
@@ -48,104 +48,99 @@ int main(int argc, char* argv[])
         mainWindow.show();
         return app.exec();
     }
-    else
+    
+    QString inputFile = "";
+    
+    bool jobExportSequence = false;
+    QString jobExportSequenceOutput = "";
+    
+    // Extracting options
+    int i;
+    for (i = 1; i < argc; i++)
     {
-        QString inputFile = "";
-
-        bool jobExportSequence = false;
-        QString jobExportSequenceOutput = "";
-
-        // Extracting options
-        int i;
-        for (i = 1; i < argc; i++)
+        if (jobExportSequence && jobExportSequenceOutput == "")
         {
-            if (jobExportSequence && jobExportSequenceOutput == "")
-            {
-                jobExportSequenceOutput = argv[i];
+            jobExportSequenceOutput = argv[i];
                 continue;
             }
-            if (QString(argv[i]) == QString("--export-sequence"))
-            {
-                jobExportSequence = true;
-                continue;
-            }
-            if (inputFile == "")
-            {
-                inputFile = QString(argv[i]);
-            }
-        }
-
-        bool error = false;
-        if ( jobExportSequence )
+        if (QString(argv[i]) == QString("--export-sequence"))
         {
-            qDebug() << "Exporting image sequence...";
-            if (inputFile.isEmpty())
-            {
-                qDebug() << "Error: No input file specified.";
-                error = true;
-            }
-            // TODO: Check if input file exists
-            if ( jobExportSequenceOutput.isEmpty())
-            {
-                qDebug() << "Error: No output file specified.";
-                error = true;
-            }
-            // TODO: Check if output path exists
-
-            if ( !error )
-            {
-                mainWindow.openFile( inputFile );
-                // Detecting format
-                QString format = "";
-                if (jobExportSequenceOutput.endsWith(".png"))
-                {
-                    format = "PNG";
-                }
-                else if (jobExportSequenceOutput.endsWith(".jpg"))
-                {
-                    format = "JPG";
-                }
-                else if (jobExportSequenceOutput.endsWith(".tif"))
-                {
-                    format = "TIF";
-                }
-                else if (jobExportSequenceOutput.endsWith(".bmp"))
-                {
-                    format = "BMP";
-                }
-                else
-                {
-                    qDebug() << "Warning: Output format is not specified or unsupported.";
-                    qDebug() << "         Using PNG.";
-                    format = "PNG";
-                }
-                mainWindow.mEditor->exportSeqCLI(jobExportSequenceOutput, format);
-                qDebug() << "Done.";
-            }
+            jobExportSequence = true;
+            continue;
         }
-        else if ( inputFile != "" )
+        if (inputFile == "")
         {
-            mainWindow.show();
-            mainWindow.openFile(inputFile);
-            return app.exec();
-        }
-        else
-        {
-            qDebug() << "Error: Invalid commandline options.";
-            error = true;
-        }
-
-        if (error)
-        {
-            qDebug() << "Syntax:";
-            qDebug() << "   " << argv[0] << "FILENAME --export-sequence PATH";
-            qDebug() << "Example:";
-            qDebug() << "   " << argv[0] << "/path/to/your/file.pcl --export-sequence /path/to/export/file.png";
-            return 1;
-        }
-        else
-        {
-            return 0;
+            inputFile = QString(argv[i]);
         }
     }
+    
+    bool error = false;
+    if ( jobExportSequence )
+    {
+        qDebug() << "Exporting image sequence...";
+        if (inputFile.isEmpty())
+        {
+            qDebug() << "Error: No input file specified.";
+            error = true;
+        }
+        // TODO: Check if input file exists
+        if ( jobExportSequenceOutput.isEmpty())
+        {
+            qDebug() << "Error: No output file specified.";
+            error = true;
+        }
+        // TODO: Check if output path exists
+        
+        if ( !error )
+        {
+            mainWindow.openFile( inputFile );
+            // Detecting format
+            QString format = "";
+            if (jobExportSequenceOutput.endsWith(".png"))
+            {
+                format = "PNG";
+            }
+            else if (jobExportSequenceOutput.endsWith(".jpg"))
+            {
+                format = "JPG";
+            }
+            else if (jobExportSequenceOutput.endsWith(".tif"))
+            {
+                format = "TIF";
+            }
+            else if (jobExportSequenceOutput.endsWith(".bmp"))
+            {
+                format = "BMP";
+            }
+            else
+            {
+                qDebug() << "Warning: Output format is not specified or unsupported.";
+                qDebug() << "         Using PNG.";
+                format = "PNG";
+            }
+            mainWindow.mEditor->exportSeqCLI(jobExportSequenceOutput, format);
+            qDebug() << "Done.";
+        }
+    }
+    else if ( inputFile != "" )
+    {
+        mainWindow.show();
+        mainWindow.openFile(inputFile);
+        return app.exec();
+    }
+    else
+    {
+        qDebug() << "Error: Invalid commandline options.";
+        error = true;
+    }
+    
+    if (error)
+    {
+        qDebug() << "Syntax:";
+        qDebug() << "   " << argv[0] << "FILENAME --export-sequence PATH";
+        qDebug() << "Example:";
+        qDebug() << "   " << argv[0] << "/path/to/your/file.pcl --export-sequence /path/to/export/file.png";
+        return 1;
+    }
+    return 0;
 }
