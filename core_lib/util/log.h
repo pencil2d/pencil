175abf00d99a031c34aee854402b527fd9e3316e
chchwy <chchwy@gmail.com>
2015-04-14 18:30:38 +0800

    Fix Travis-ci build fail

--- core_lib/util/log.h
+++ core_lib/util/log.h
@@ -3,12 +3,14 @@
 
 #if QT_VERSION >= QT_VERSION_CHECK( 5, 2, 0 )
 	#include<QLoggingCategory>
+    #define ENABLE_DEBUG_LOG( Logger, OnOff ) Logger.setEnabled( QtDebugMsg, OnOff )
 #endif
 
 #if QT_VERSION < QT_VERSION_CHECK( 5, 2, 0 )
     #define QLoggingCategory QString
     #define qCDebug( C ) qDebug()
     #define qCWarning( C ) qWarning()
+    #define ENABLE_DEBUG_LOG( Logger, OnOff )
 #endif
 
 #endif // LOG_H
