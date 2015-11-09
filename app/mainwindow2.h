2b5e0684be1b30c646cb4836e2d2b5ec69503d1d
Matt <chchwy@gmail.com>
2015-11-09 11:45:00 +0800

    copyright.

--- app/mainwindow2.h
+++ app/mainwindow2.h
@@ -1,3 +1,20 @@
+/*
+
+Pencil - Traditional Animation Software
+Copyright (C) 2005-2007 Patrick Corrieri & Pascal Naidon
+Copyright (C) 2008-2009 Mj Mendoza IV
+Copyright (C) 2011-2015 Matt Chiawen Chang
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation;
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+*/
 #ifndef MAINWINDOW2_H
 #define MAINWINDOW2_H
 
