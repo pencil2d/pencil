b3ee0505092a8a278fef3341dc1cfbf8fb6e91aa
scribblemaniac <scribblemaniac@users.noreply.github.com>
2016-08-31 22:25:56 -0600

    Various CLI improvements
    - Add support for multiple outputs
    - Fix camera layer size not being used
    - Add `--help` and `--version` flags
    - Prevent GUI from opening when rendering from command line

--- app/main.cpp
+++ app/main.cpp
@@ -54,6 +54,96 @@ void installTranslator( PencilApplication& app )
     qDebug() << "Install translation = " << b;
 }
 
+bool parseArguments( QStringList args, QString & inputPath, QStringList & outputPaths )
+{
+#if QT_VERSION >= QT_VERSION_CHECK( 5, 2, 0 )
+#import <QCommandLineParser>
+#import <QCommandLineOption>
+    QCommandLineParser parser;
+
+    parser.setApplicationDescription( PencilApplication::tr("Pencil2D is an animation/drawing software for Mac OS X, Windows, and Linux. It lets you create traditional hand-drawn animation (cartoon) using both bitmap and vector graphics.") );
+    parser.addHelpOption();
+    parser.addVersionOption();
+    parser.addPositionalArgument( "input", PencilApplication::tr( "Path to the input pencil file." ) );
+
+    QCommandLineOption exportSeqOption( QStringList() << "o" << "export-sequence",
+                                        PencilApplication::tr( "Render the file to <output_path>" ),
+                                        PencilApplication::tr( "output_path" ) );
+    parser.addOption( exportSeqOption );
+
+    parser.process( args );
+
+    QStringList posArgs = parser.positionalArguments();
+    if ( !posArgs.isEmpty() )
+    {
+        inputPath = posArgs.at(0);
+    }
+
+    outputPaths = parser.values( exportSeqOption );
+    return true;
+#else
+    // Extracting options
+    bool error = false, help = false;
+    for ( int i = 1; i < args.length(); i++ ) // TODO: use iterator instead
+    {
+        if ( args[i] == "-NSDocumentRevisionsDebugMode")
+        {
+            // Ignore the next option
+            i++;
+        }
+        else if ( args[i] == "-o" || args[i] == "--export-sequence" )
+        {
+            if( ++i < args.length() )
+            {
+                // Output path is at args[i]
+                outputPaths.append(args[i]);
+            }
+            else
+            {
+                // Error, no output path specified
+                qDebug() << PencilApplication::tr( "Error: no output path specified" );
+                error = true;
+            }
+        }
+        else if ( args[i] == "-h" || args[i] == "--help" )
+        {
+            help = true;
+        }
+        else if ( args[i] == "-v" || args[i] == "--version" )
+        {
+            return false;
+        }
+        else if ( args[i].startsWith( "-" ) )
+        {
+            qDebug() << "Error: Unknown option '" << args[i] << "'";
+            error = true;
+        }
+        else if ( inputPath.isEmpty() )
+        {
+            // Positional argument: input file path
+            inputPath = args[i];
+        }
+    }
+
+    if (error || help) {
+        qDebug() << "Usage: ./Pencil2D [options] input" << std::endl
+                 << "Pencil2D is an animation/drawing software for Mac OS X, Windows, and Linux. It lets you create traditional hand-drawn animation (cartoon) using both bitmap and vector graphics." << std::endl
+                 << std::endl
+                 << "Options:" << std::endl
+                 << "  -h, --help                            Displays this help." << std::endl
+                 << "  -v, --version                        Displays version information." << std::endl
+                 << "  -o, --export-sequence <output_path>  Render the file to <output_path>" << std::endl
+                 << std::endl
+                 << "Arguments:" << std::endl
+                 << "  input                                Path to the input pencil file.";
+        return help;
+    }
+
+    return true;
+#endif
+}
+
+
 int main(int argc, char* argv[])
 {
     PencilApplication app( argc, argv );
@@ -66,105 +156,68 @@ int main(int argc, char* argv[])
     QObject::connect(&app, &PencilApplication::openFileRequested, &mainWindow, &MainWindow2::openDocument);
     //QObject::connect(&app, SIGNAL(openFileRequested(QString)), &mainWindow, SLOT(openDocument(QString)));
     app.emitOpenFileRequest();
-    
-    if ( argc == 1 || (argc > 1 && strcmp( argv[1], "-NSDocumentRevisionsDebugMode" ) == 0)  )
+
+    if ( argc == 1 || ( ( argc == 2 || argc == 3 ) && QString(argv[1]) == "-NSDocumentRevisionsDebugMode" ) )
     {
         mainWindow.show();
         return app.exec();
     }
 
-    QString inputFile;
+    QString inputPath;
+    QStringList outputPaths;
 
-    bool jobExportSequence = false;
-    QString jobExportSequenceOutput = "";
-
-    // Extracting options
-    int i;
-    for ( i = 1; i < argc; ++i )
+    if ( !parseArguments( app.arguments(), inputPath, outputPaths ) )
     {
-        if (jobExportSequence && jobExportSequenceOutput == "")
-        {
-            jobExportSequenceOutput = argv[i];
-            continue;
-        }
-        if ( QString(argv[i]) == "--export-sequence" )
-        {
-            jobExportSequence = true;
-            continue;
-        }
-        if (inputFile == "")
-        {
-            inputFile = QString(argv[i]);
-        }
-    }
-
-    bool error = false;
-    if ( jobExportSequence )
-    {
-        std::cout << "Exporting image sequence..." << std::endl;
-        if (inputFile.isEmpty())
-        {
-            qDebug() << "Error: No input file specified.";
-            error = true;
-        }
-        // TODO: Check if input file exists
-        if ( jobExportSequenceOutput.isEmpty())
-        {
-            qDebug() << "Error: No output file specified.";
-            error = true;
-        }
-        // TODO: Check if output path exists
-
-        if ( !error )
-        {
-            mainWindow.openFile( inputFile );
-            // Detecting format
-            QString format = "";
-            if (jobExportSequenceOutput.endsWith(".png"))
-            {
-                format = "PNG";
-            }
-            else if (jobExportSequenceOutput.endsWith(".jpg"))
-            {
-                format = "JPG";
-            }
-            else if (jobExportSequenceOutput.endsWith(".tif"))
-            {
-                format = "TIF";
-            }
-            else if (jobExportSequenceOutput.endsWith(".bmp"))
-            {
-                format = "BMP";
-            }
-            else
-            {
-                qDebug() << "Warning: Output format is not specified or unsupported.";
-                qDebug() << "         Using PNG.";
-                format = "PNG";
-            }
-            mainWindow.mEditor->exportSeqCLI(jobExportSequenceOutput, format);
-            qDebug() << "Done.";
-        }
-    }
-    else if ( !inputFile.isEmpty() )
-    {
-        mainWindow.show();
-        mainWindow.openFile(inputFile);
-        return app.exec();
-    }
-    else
-    {
-        qDebug() << "Error: Invalid commandline options.";
-        error = true;
-    }
-
-    if (error)
-    {
-        qDebug() << "Syntax:";
-        qDebug() << "   " << argv[0] << "FILENAME --export-sequence PATH";
-        qDebug() << "Example:";
-        qDebug() << "   " << argv[0] << "/path/to/your/file.pcl --export-sequence /path/to/export/file.png";
         return 1;
     }
+
+    // If there are no output paths, open up the GUI (to the input path if there is one)
+    if ( outputPaths.isEmpty() )
+    {
+        mainWindow.show();
+        if( !inputPath.isEmpty() )
+        {
+            mainWindow.openFile(inputPath);
+        }
+        return app.exec();
+    }
+    else if ( inputPath.isEmpty() )
+    {
+        // Error if there are output paths without an input path
+        qDebug() << PencilApplication::tr( "Error: No input file specified." );
+        return 1;
+    }
+
+    std::cout << "Exporting image sequence..." << std::endl;
+    // TODO: Check if input file exists
+    for ( int i = 0; i < outputPaths.length(); i++ )
+    {
+        // TODO: Check if output path exists
+        mainWindow.openFile( inputPath );
+
+        // Detect format
+        QString format;
+        QMap<QString, QString> extensionMapping;
+        extensionMapping[ "png" ] = "PNG";
+        extensionMapping[ "jpg" ] = "JPG";
+        extensionMapping[ "jpeg" ] = "JPG";
+        extensionMapping[ "tif" ] = "TIF";
+        extensionMapping[ "tiff" ] = "TIF";
+        extensionMapping[ "bmp" ] = "BMP";
+        QString extension = outputPaths[i].mid( outputPaths[i].lastIndexOf( "." ) + 1 ).toLower();
+        //qDebug() << "Ext: " << outputPaths[i].lastIndexOf(".") << " " << extension << " " << outputPaths[i];
+        if ( inputPath.contains(".") && extensionMapping.contains( extension ) )
+        {
+            format = extensionMapping[extension];
+        }
+        else {
+            qDebug() << "Warning: Output format is not specified or unsupported. Using PNG.";
+            format = "PNG";
+        }
+
+        mainWindow.mEditor->exportSeqCLI(outputPaths[i], format);
+    }
+    qDebug() << "Done.";
+
     return 0;
 }
