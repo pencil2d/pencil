5da5e8202436014d99287b7c61703ec53e47e85b
Jakob Gahde <j5lx@fmail.co.uk>
2017-05-15 21:55:17 +0200

    Workaround for probonopd/linuxdeployqt#83

---
+++ util/linux-ffmpeg-wrapper.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# This script serves as a workaround for probonopd/linuxdeployqt#83 in Linux builds.
+
+prefix="$(dirname $(dirname $(readlink -e $0)))"
+export LD_LIBRARY_PATH="${prefix}/lib:${LD_LIBRARY_PATH}"
+"${prefix}/plugins/ffmpeg.bin" $@
