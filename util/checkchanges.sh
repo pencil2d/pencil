ecd5d2b0b078076639e37e4383f3ef91d318f7d8
Matt Chang <chchwy@gmail.com>
2017-09-22 21:49:06 +1000

    Debug messages

--- util/checkchanges.sh
+++ util/checkchanges.sh
@@ -13,7 +13,6 @@ if [[ $branch == "master" ]]; then
   CHANGED_FILES=`git diff --name-only HEAD^ HEAD`
 fi
 
-echo "check against: $CHANGED_FILES"
 echo "branch is: $branch"
 
 ONLY_READMES=True
@@ -23,18 +22,19 @@ SH=".sh"
 PY=".py"
 PS1=".ps1"
 
-printf "git diff --name-only origin/master...${TRAVIS_COMMIT}"
-printf ${CHANGED_FILES}
-printf "==============="
+printf "git diff --name-only origin/master...%s\n" "${TRAVIS_COMMIT}"
+printf "%s\n" "${CHANGED_FILES}"
+printf "=============\n"
 
 for CHANGED_FILE in $CHANGED_FILES; do
+  printf "Check ${CHANGED_FILES}"
   if ! [[ $CHANGED_FILE =~ $MD || 
   		  $CHANGED_FILE =~ $YAML || 
   		  $CHANGED_FILE =~ $SH || 
   		  $CHANGED_FILE =~ $PY ||
   		  $CHANGED_FILE =~ $PS1 ]] ; then
     ONLY_READMES=False
-	printf "Gotcha! %s\n" "${CHANGED_FILE}"
+    printf "Gotcha! %s\n" "${CHANGED_FILE}"
     break
   fi
 done
