65698cd450a5f3248ad4ed43dd7de0e7f9371b02
scribblemaniac <scribblemaniac@users.noreply.github.com>
2017-11-03 11:43:13 -0600

    Fix error report when making a new file in a read-only directory.

--- core_lib/structure/filemanager.cpp
+++ core_lib/structure/filemanager.cpp
@@ -197,9 +197,9 @@ Status FileManager::save( Object* object, QString strFileName )
                        tr( "Invalid Save Path" ),
                        tr( "The file path you have specified (\"%1\") points to a directory, so the file cannot be saved." ).arg( fileInfo.absoluteFilePath() ) );
     }
-    if ( fileInfo.exists() && !fileInfo.isWritable() )
+    if ( !fileInfo.isWritable() )
     {
-        debugDetails << "strFileName points to a file that is not writable";
+        debugDetails << "strFileName points to a location that is not writable";
         return Status( Status::INVALID_ARGUMENT,
                        debugDetails ,
                        tr( "Invalid Save Path" ),
