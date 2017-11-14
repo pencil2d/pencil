4fb89ae4163f83e47ace165dbae15cd6a00d10a9
Matt Chang <chchwy@gmail.com>
2017-11-14 13:20:42 +1100

    WIP test_layer

--- tests/test_layer.cpp
+++ tests/test_layer.cpp
@@ -1,5 +1,8 @@
 #include "test_layer.h"
 
+#include <memory>
+#include "catch.hpp"
+
 #include "layer.h"
 #include "layerbitmap.h"
 #include "layervector.h"
@@ -7,103 +10,188 @@
 #include "layersound.h"
 #include "object.h"
 #include "util.h"
-#include <memory>
 
-TestLayer::TestLayer()
+
+TEST_CASE("LayerType")
 {
+    Object* object = new Object;
+
+    SECTION("Bitmap Layer")
+    {
+        Layer* bitmapLayer = new LayerBitmap(object);
+        REQUIRE(bitmapLayer->type() == Layer::BITMAP);
+        delete bitmapLayer;
+    }
+    SECTION("Vector Layer")
+    {
+        Layer* vecLayer = new LayerVector(object);
+        REQUIRE(vecLayer->type() == Layer::VECTOR);
+        delete vecLayer;
+    }
+    SECTION("Camera Layer")
+    {
+        Layer* cameraLayer = new LayerCamera(object);
+        REQUIRE(cameraLayer->type() == Layer::CAMERA);
+        delete cameraLayer;
+    }
+    SECTION("Sound Layer")
+    {
+        Layer* soundLayer = new LayerSound(object);
+        REQUIRE(soundLayer->type() == Layer::SOUND);
+        delete soundLayer;
+    }
+
+    delete object;
 }
 
-void TestLayer::initTestCase()
+SCENARIO("Add key frames into a Layer", "[Layer]")
 {
-    m_pObject = new Object();
-    m_pObject->init();
+    Object* object = new Object;
+
+    GIVEN("A Bitmap Layer")
+    {
+        Layer* layer = new LayerBitmap(object);
+
+        REQUIRE(layer->addNewEmptyKeyAt(0) == false); // first key position is 1.
+        REQUIRE(layer->keyFrameCount() == 0);
+
+        WHEN("Add a keyframe")
+        {
+            REQUIRE(layer->addNewEmptyKeyAt(1) == true);
+
+            THEN("keyframe can be found in the layer")
+            {
+                REQUIRE(layer->getMaxKeyFramePosition() == 1);
+                REQUIRE(layer->keyFrameCount() == 1);
+                REQUIRE(layer->getKeyFrameAt(1) != nullptr);
+            }
+        }
+
+        WHEN("Add 2 keyframes")
+        {
+            REQUIRE(layer->addNewEmptyKeyAt(1) == true);
+            REQUIRE(layer->addNewEmptyKeyAt(3) == true);
+
+            THEN("2 keyframes are in the layer")
+            {
+                REQUIRE(layer->getMaxKeyFramePosition() == 3);
+                REQUIRE(layer->getKeyFrameAt(3) != nullptr);
+                REQUIRE(layer->keyFrameCount() == 2);
+            }
+        }
+        delete layer;
+    }
+
+    GIVEN("A Vector Layer")
+    {
+        Layer* layer = new LayerVector(object);
+
+        REQUIRE(layer->addNewEmptyKeyAt(0) == false); // first key position is 1.
+        REQUIRE(layer->keyFrameCount() == 0);
+
+        WHEN("Add a keyframe")
+        {
+            REQUIRE(layer->addNewEmptyKeyAt(1) == true);
+
+            THEN("keyframe can be found in the layer")
+            {
+                REQUIRE(layer->getMaxKeyFramePosition() == 1);
+                REQUIRE(layer->keyFrameCount() == 1);
+                REQUIRE(layer->getKeyFrameAt(1) != nullptr);
+            }
+        }
+        delete layer;
+    }
+
+    GIVEN("A Camera Layer")
+    {
+        Layer* layer = new LayerCamera(object);
+
+        REQUIRE(layer->addNewEmptyKeyAt(0) == false); // first key position is 1.
+        REQUIRE(layer->keyFrameCount() == 0);
+
+        WHEN("Add a keyframe")
+        {
+            REQUIRE(layer->addNewEmptyKeyAt(1) == true);
+
+            THEN("keyframe can be found in the layer")
+            {
+                REQUIRE(layer->getMaxKeyFramePosition() == 1);
+                REQUIRE(layer->keyFrameCount() == 1);
+                REQUIRE(layer->getKeyFrameAt(1) != nullptr);
+            }
+        }
+        delete layer;
+    }
+    delete object;
 }
 
-void TestLayer::cleanupTestCase()
+TEST_CASE("Test Layer::keyExists()", "[Layer]")
 {
-    delete m_pObject;
+    Object* obj = new Object;
+
+    SECTION("Fresh new Layer")
+    {
+        Layer* layer = obj->addNewBitmapLayer();
+        REQUIRE(layer->keyExists(1) == true); // there is a frame at 1 in default.
+
+    }
+    SECTION("Key exists at 15")
+    {
+        Layer* layer = obj->addNewBitmapLayer();
+        REQUIRE(layer->addNewEmptyKeyAt(15));
+        REQUIRE(layer->keyExists(16) == false);
+        REQUIRE(layer->keyExists(15) == true);
+        REQUIRE(layer->keyExists(14) == false);
+        REQUIRE(layer->keyExists(13) == false);
+        REQUIRE(layer->keyExists(12) == false);
+    }
+    SECTION("Key exists 10 20 30")
+    {
+        Layer* layer = obj->addNewBitmapLayer();
+
+        REQUIRE(layer->addNewEmptyKeyAt(10));
+        REQUIRE(layer->keyExists(10) == true);
+
+        REQUIRE(layer->addNewEmptyKeyAt(20));
+        REQUIRE(layer->keyExists(20) == true);
+
+        REQUIRE(layer->addNewEmptyKeyAt(30));
+        REQUIRE(layer->keyExists(30) == true);
+
+        // test false case
+        REQUIRE(layer->keyExists(0) == false);
+        REQUIRE(layer->keyExists(21) == false);
+        REQUIRE(layer->keyExists(31) == false);
+        REQUIRE(layer->keyExists(1000) == false);
+        REQUIRE(layer->keyExists(-333) == false);
+    }
+
+    delete obj;
 }
 
-void TestLayer::testCase1()
+TEST_CASE("Test Layer::firstKeyFramePosition()")
 {
-    QVERIFY2( true, "Failure" );
-}
-
-void TestLayer::testLayerType()
-{
-    std::unique_ptr< Layer > bitmapLayer( new LayerBitmap( m_pObject ) );
-    QVERIFY( bitmapLayer->type() == Layer::BITMAP );
-
-    std::unique_ptr< Layer > vecLayer( new LayerVector( m_pObject ) );
-    QVERIFY( vecLayer->type() == Layer::VECTOR );
-
-    std::unique_ptr< Layer > cameraLayer( new LayerCamera( m_pObject ) );
-    QVERIFY( cameraLayer->type() == Layer::CAMERA );
-
-    std::unique_ptr< Layer > soundLayer( new LayerSound( m_pObject ) );
-    QVERIFY( soundLayer->type() == Layer::SOUND );
-}
-
-void TestLayer::testAddNewKeyAtBitmap()
-{
-    std::unique_ptr< Layer > spLayer( new LayerBitmap( m_pObject ) );
+    Object* obj = new Object;
     
-    bool bOK = false;
+    SECTION("Key at 1")
+    {        
+        Layer* layer = obj->addNewBitmapLayer();
+        REQUIRE(layer->firstKeyFramePosition() == 1);
+        
+        layer->addNewEmptyKeyAt(99);
+        REQUIRE(layer->firstKeyFramePosition() == 1);
 
-    bOK = spLayer->addNewEmptyKeyAt( 0 );
-    QVERIFY2( bOK == false, "Frame Number must > 0." );
-
-    QVERIFY( spLayer->addNewEmptyKeyAt( 1 ) );
-
-    QCOMPARE( spLayer->addNewEmptyKeyAt( 2 ), true );
-    QCOMPARE( spLayer->getMaxKeyFramePosition(), 2 );
+        layer->moveKeyFrameForward(1);
+        REQUIRE(layer->firstKeyFramePosition() == 2);
+    }
 }
 
-void TestLayer::testAddNewKeyAtVector()
-{
-    Layer* layer = m_pObject->addNewVectorLayer();;
-    OnScopeExit( m_pObject->deleteLayer( layer ) );
-
-    bool bOK = false;
-
-    bOK = layer->addNewEmptyKeyAt( 0 );
-    QVERIFY2( bOK == false, "Frame Number must > 0." );
-
-    QVERIFY( layer->addNewEmptyKeyAt( 1 ) == false );
-    
-    QCOMPARE( layer->addNewEmptyKeyAt( 2 ), true );
-    QCOMPARE( layer->getMaxKeyFramePosition(), 2 );
-}
-
-void TestLayer::testHasKeyFrameAtPosition()
-{
-    Layer* layer = m_pObject->addNewBitmapLayer();;
-    OnScopeExit( m_pObject->deleteLayer( layer ) );
-
-    QCOMPARE( layer->keyExists( 1 ), true ); // there is a frame at 1 in default.
-
-    QVERIFY( layer->addNewEmptyKeyAt( 15 ) );
-    QCOMPARE( layer->keyExists( 15 ), true );
-    QCOMPARE( layer->keyExists( 10 ), false );
-
-    QVERIFY( layer->addNewEmptyKeyAt( 10 ) );
-    QCOMPARE( layer->keyExists( 10 ), true );
-
-    // test false case
-    QCOMPARE( layer->keyExists( 0 ), false );
-    QCOMPARE( layer->keyExists( 1000 ), false );
-    QCOMPARE( layer->keyExists( -333 ), false );
-}
+/*
 
 void TestLayer::testGetFirstFramePosition()
 {
-    Layer* layer = m_pObject->addNewBitmapLayer();;
-    OnScopeExit( m_pObject->deleteLayer( layer ) );
 
-    QCOMPARE( layer->firstKeyFramePosition(), 1 );
-    layer->addNewEmptyKeyAt( 99 );
-
-    QCOMPARE( layer->firstKeyFramePosition(), 1 );
 }
 
 void TestLayer::testGetMaxFramePosition()
@@ -195,3 +283,4 @@ void TestLayer::testNextKeyFramePosition()
     QCOMPARE( pLayer->getNextKeyFramePosition( 1 ), 5 );
     QCOMPARE( pLayer->getNextKeyFramePosition( 2 ), 5 );
 }
+*/
