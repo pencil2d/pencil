8469370cda34f49999651bdeea44627827a2a658
Matt Chang <chchwy@gmail.com>
2017-12-18 14:06:19 +1100

    NB upload script will delete any file older than 90 days automatically

--- util/nightly-build-upload.py
+++ util/nightly-build-upload.py
@@ -8,6 +8,7 @@
 import json
 import os
 import sys
+import datetime
 
 from apiclient import discovery
 from apiclient.http import MediaFileUpload
@@ -24,12 +25,31 @@ if len(sys.argv) != 3:
   sys.exit(1)
 [parent, file] = sys.argv[1:]
 
+# build google drive service
 scopes = ['https://www.googleapis.com/auth/drive']
 keyfile_dict = json.loads(private_key)
 credentials = ServiceAccountCredentials.from_json_keyfile_dict(keyfile_dict, scopes=scopes)
 http = credentials.authorize(Http())
 drive_service = discovery.build('drive', 'v3', http=http)
 
+# upload nightly build
 file_metadata = { 'name' : os.path.basename(file), 'parents': [parent] }
 media = MediaFileUpload(file)
 file = drive_service.files().create(body=file_metadata, media_body=media).execute()
+
+# delete files older than 90 days
+response = drive_service.files().list(q="mimeType='application/zip'", spaces='drive').execute()
+
+for f in response.get('files'):
+  s = f.get('name').split('.')[0]
+  s = s.split('-')
+  upload_date = datetime.datetime(int(s[2]), int(s[3]), int(s[4]))
+
+  check_date = datetime.datetime.now() + datetime.timedelta(-90)
+
+  if upload_date < check_date:
+    print("Found a file older than 90 days")
+    print("File ID:", f.get('id'))
+    print("File Name:", f.get('name'))
+    print("Upload date:", upload_date)
+    drive_service.files().delete(fileId=f.get('id')).execute()
