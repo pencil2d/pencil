d31eb9ff0077a0990038c638a949faebe42e9014
scribblemaniac <scribblemaniac@users.noreply.github.com>
2018-05-17 00:03:18 -0600

    Remove oldColor argument of floodFill, since it isn't used

--- core_lib/src/graphics/bitmap/bitmapimage.cpp
+++ core_lib/src/graphics/bitmap/bitmapimage.cpp
@@ -563,14 +563,10 @@ bool BitmapImage::compareColor(QRgb color1, QRgb color2, int tolerance)
 void BitmapImage::floodFill(BitmapImage* targetImage,
                             QRect cameraRect,
                             QPoint point,
-                            QRgb oldColor,
                             QRgb newColor,
                             int tolerance)
 {
-    if (oldColor == newColor)
-        return;
-    
-    oldColor = targetImage->pixel(point);
+    QRgb oldColor = targetImage->pixel(point);
     oldColor = qRgba(qRed(oldColor), qGreen(oldColor), qBlue(oldColor), qAlpha(oldColor));
 
     // Preparations
