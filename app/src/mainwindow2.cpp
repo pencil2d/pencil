c1b9a41d802bfdaec463dda27ef6a86ac3c42a3c
Yan Yan Harris <yharriscollege@gmail.com>
2019-04-28 21:43:01 -0700

    Fixed problem #1170 where the redo text looked like a debug log. Problem in MainWindow2.cpp line 1232

--- app/src/mainwindow2.cpp
+++ app/src/mainwindow2.cpp
@@ -1229,7 +1229,7 @@ void MainWindow2::undoActSetText()
 
     if (mEditor->mBackupIndex + 2 < mEditor->mBackupList.size())
     {
-        ui->actionRedo->setText(QString("%1   %2 %3").arg("Redo", "Menu item text")
+        ui->actionRedo->setText(QString("%1   %2 %3").arg(tr("Redo", "Menu item text"))
                                 .arg(QString::number(mEditor->mBackupIndex + 2))
                                 .arg(mEditor->mBackupList.at(mEditor->mBackupIndex + 1)->undoText));
         ui->actionRedo->setEnabled(true);
