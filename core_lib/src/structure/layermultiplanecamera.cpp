778ce5b97ab7691257d8c51b6a0a4e473c32c92a
David Lamhauge <davidlamhauge@gmail.com>
2019-11-08 06:15:54 +0100

    Deleted two irrelevant files

--- core_lib/src/structure/layermultiplanecamera.cpp
+++
@@ -1,6 +0,0 @@
-#include "layermultiplanecamera.h"
-
-LayerMultiPlaneCamera::LayerMultiPlaneCamera(Object *object) :Layer(object, LAYER_TYPE::MULTIPLANCAMERA)
-{
-    setName(tr("Multiplane Camera"));
-}
