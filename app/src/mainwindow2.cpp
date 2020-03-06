a181fdeee27ab3f0460d297ac2cd92621aeba2ee
CandyFace <CandyFace18@gmail.com>
2020-03-06 23:43:12 +0100

    #1307: fix path not being reset

--- app/src/mainwindow2.cpp
+++ app/src/mainwindow2.cpp
@@ -1056,6 +1056,7 @@ bool MainWindow2::newObjectFromPresets(int presetIndex)
         object->createDefaultLayers();
     }
     mEditor->setObject(object);
+    object->setFilePath(QString());
     return true;
 }
 
