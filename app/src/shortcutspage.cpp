1fb472d2b4cf4b611a9a0e04ec3b42bbbbe9c88d
Jakob Gahde <j5lx@fmail.co.uk>
2020-05-06 22:08:51 +0200

    Make shortcut names translatable

--- app/src/shortcutspage.cpp
+++ app/src/shortcutspage.cpp
@@ -32,6 +32,8 @@ GNU General Public License for more details.
 static const int ACT_NAME_COLUMN = 0;
 static const int KEY_SEQ_COLUMN  = 1;
 
+static QString getHumanReadableShortcutName(const QString&);
+
 ShortcutsPage::ShortcutsPage( QWidget* parent )
     : QWidget(parent),
     ui( new Ui::ShortcutsPage )
@@ -87,7 +89,7 @@ void ShortcutsPage::keyCapLineEditTextChanged()
     QStandardItem* actionItem = m_treeModel->item(row, ACT_NAME_COLUMN);
     QStandardItem* keyseqItem = m_treeModel->item(row, KEY_SEQ_COLUMN);
 
-    QString strCmdName = QString("Cmd%1").arg( actionItem->text() );
+    QString strCmdName = actionItem->data().toString();
     QString strKeySeq  = keySeqence.toString( QKeySequence::PortableText );
 
     QSettings setting( PENCIL2D, PENCIL2D );
@@ -238,15 +240,13 @@ void ShortcutsPage::treeModelLoadShortcutsSetting()
         //convert to native format
         strKeySequence = QKeySequence(strKeySequence).toString(QKeySequence::NativeText);
 
-        // strip the first 3 chars "Cmd"
-        QStringRef strHumanReadCmdName (&strCmdName, 3, strCmdName.size() - 3);
-
         if (m_treeModel->item(row , ACT_NAME_COLUMN) == nullptr)
             m_treeModel->setItem(row, ACT_NAME_COLUMN, new QStandardItem());
         if (m_treeModel->item(row, KEY_SEQ_COLUMN) == nullptr)
             m_treeModel->setItem(row, KEY_SEQ_COLUMN, new QStandardItem());
 
-        m_treeModel->item(row, ACT_NAME_COLUMN)->setText(strHumanReadCmdName.toString());
+        m_treeModel->item(row, ACT_NAME_COLUMN)->setData(strCmdName);
+        m_treeModel->item(row, ACT_NAME_COLUMN)->setText(getHumanReadableShortcutName(strCmdName));
         m_treeModel->item(row, ACT_NAME_COLUMN)->setEditable(false);
         m_treeModel->item(row, KEY_SEQ_COLUMN)->setText(strKeySequence);
         m_treeModel->item(row, KEY_SEQ_COLUMN)->setEditable(false);
@@ -268,7 +268,7 @@ void ShortcutsPage::clearButtonClicked()
     int row = m_currentItemIndex.row();
     QStandardItem* actionItem = m_treeModel->item(row, ACT_NAME_COLUMN);
 
-    QString strCmdName = QString("shortcuts/Cmd%1").arg( actionItem->text() );
+    QString strCmdName = QString("shortcuts/%1").arg( actionItem->data().toString() );
 
     QSettings setting( PENCIL2D, PENCIL2D );
     setting.setValue( strCmdName, "" );
@@ -277,4 +277,98 @@ void ShortcutsPage::clearButtonClicked()
     ui->keySequenceEdit->clear();
 
     treeModelLoadShortcutsSetting();
+}
+
+/**
+ * Translates internal to human-readable shortcut names
+ *
+ * @param[in] cmdName The name of the setting corresponding to the shortcut
+ * @return The translated, human-readable name of the shortcut
+ */
+static QString getHumanReadableShortcutName(const QString& cmdName)
+{
+    static QHash<QString, QString> humanReadableShortcutNames = QHash<QString, QString>{
+        {"CmdAddFrame", QObject::tr("Add Frame")},
+        {"CmdClearFrame", QObject::tr("Clear Frame")},
+        {"CmdCopy", QObject::tr("Copy")},
+        {"CmdCut", QObject::tr("Cut")},
+        {"CmdDeleteCurrentLayer", QObject::tr("Delete Current Layer")},
+        {"CmdDeselectAll", QObject::tr("Deselect All")},
+        {"CmdDuplicateFrame", QObject::tr("Duplicate Frame")},
+        {"CmdExit", QObject::tr("Exit")},
+        {"CmdExportImage", QObject::tr("Export Image")},
+        {"CmdExportImageSequence", QObject::tr("Export Image Sequence")},
+        {"CmdExportMovie", QObject::tr("Export Movie")},
+        {"CmdExportPalette", QObject::tr("Export Palette")},
+        {"CmdExportSound", QObject::tr("Export Sound")},
+        {"CmdFlipHorizontal", QObject::tr("Horizontal Flip")},
+        {"CmdFlipInBetween", QObject::tr("Flip In-Between")},
+        {"CmdFlipRolling", QObject::tr("Flip Rolling")},
+        {"CmdFlipVertical", QObject::tr("Vertical Flip")},
+        {"CmdGotoNextFrame", QObject::tr("Next Frame")},
+        {"CmdGotoNextKeyFrame", QObject::tr("Next Keyframe")},
+        {"CmdGotoPreviousFrame", QObject::tr("Previous Frame")},
+        {"CmdGotoPreviousKeyFrame", QObject::tr("Previous Keyframe")},
+        {"CmdGrid", QObject::tr("Toggle Grid")},
+        {"CmdImportImage", QObject::tr("Import Image")},
+        {"CmdImportImageSequence", QObject::tr("Import Image Sequence")},
+        {"CmdImportSound", QObject::tr("Import Sound")},
+        {"CmdLayerVisibilityAll", QObject::tr("Show All Layers")},
+        {"CmdLayerVisibilityCurrentOnly", QObject::tr("Show Current Layer Only")},
+        {"CmdLayerVisibilityRelative", QObject::tr("Show Layers Relative to Current Layer")},
+        {"CmdLoop", QObject::tr("Toggle Loop")},
+        {"CmdMoveFrameBackward", QObject::tr("Move Frame Backward")},
+        {"CmdMoveFrameForward", QObject::tr("Move Frame Forward")},
+        {"CmdNewBitmapLayer", QObject::tr("New Bitmap Layer")},
+        {"CmdNewCameraLayer", QObject::tr("New Camera Layer")},
+        {"CmdNewFile", QObject::tr("New File")},
+        {"CmdNewSoundLayer", QObject::tr("New Sound Layer")},
+        {"CmdNewVectorLayer", QObject::tr("New Vector Layer")},
+        {"CmdOnionSkinNext", QObject::tr("Toggle Next Onion Skin")},
+        {"CmdOnionSkinPrevious", QObject::tr("Toggle Previous Onion Skin")},
+        {"CmdOpenFile", QObject::tr("Open File")},
+        {"CmdPaste", QObject::tr("Paste")},
+        {"CmdPlay", QObject::tr("Play/Stop")},
+        {"CmdPreferences", QObject::tr("Preferences")},
+        {"CmdRedo", QObject::tr("Redo")},
+        {"CmdRemoveFrame", QObject::tr("Remove Frame")},
+        {"CmdResetWindows", QObject::tr("Reset Windows")},
+        {"CmdResetZoomRotate", QObject::tr("Reset Zoom/Rotate")},
+        {"CmdRotateAntiClockwise", QObject::tr("Rotate Anticlockwise")},
+        {"CmdRotateClockwise", QObject::tr("Rotate Clockwise")},
+        {"CmdSaveAs", QObject::tr("Save File As")},
+        {"CmdSaveFile", QObject::tr("Save File")},
+        {"CmdSelectAll", QObject::tr("Select All")},
+        {"CmdToggleColorInspector", QObject::tr("Toggle Color Inspector Window Visibility")},
+        {"CmdToggleColorLibrary", QObject::tr("Toggle Color Palette Window Visibility")},
+        {"CmdToggleColorWheel", QObject::tr("Toggle Color Box Window Visibility")},
+        {"CmdToggleDisplayOptions", QObject::tr("Toggle Display Window Visibility")},
+        {"CmdToggleOnionSkin", QObject::tr("Toggle Onion Skins Window Visibility")},
+        {"CmdToggleTimeline", QObject::tr("Toggle Timeline Window Visibility")},
+        {"CmdToggleToolBox", QObject::tr("Toggle Tools Window Visibility")},
+        {"CmdToggleToolOptions", QObject::tr("Toggle Options Window Visibility")},
+        {"CmdToolBrush", QObject::tr("Brush Tool")},
+        {"CmdToolBucket", QObject::tr("Bucket Tool")},
+        {"CmdToolEraser", QObject::tr("Eraser Tool")},
+        {"CmdToolEyedropper", QObject::tr("Eyedropper Tool")},
+        {"CmdToolHand", QObject::tr("Hand Tool")},
+        {"CmdToolMove", QObject::tr("Move Tool")},
+        {"CmdToolPen", QObject::tr("Pen Tool")},
+        {"CmdToolPencil", QObject::tr("Pencil Tool")},
+        {"CmdToolPolyline", QObject::tr("Polyline Tool")},
+        {"CmdToolSelect", QObject::tr("Select Tool")},
+        {"CmdToolSmudge", QObject::tr("Smudge Tool")},
+        {"CmdUndo", QObject::tr("Undo")},
+        {"CmdZoom100", QObject::tr("Set Zoom to 100%")},
+        {"CmdZoom200", QObject::tr("Set Zoom to 200%")},
+        {"CmdZoom25", QObject::tr("Set Zoom to 25%")},
+        {"CmdZoom300", QObject::tr("Set Zoom to 300%")},
+        {"CmdZoom33", QObject::tr("Set Zoom to 33%")},
+        {"CmdZoom400", QObject::tr("Set Zoom to 400%")},
+        {"CmdZoom50", QObject::tr("Set Zoom to 50%")},
+        {"CmdZoomIn", QObject::tr("Zoom In")},
+        {"CmdZoomOut", QObject::tr("Zoom Out")},
+    };
+
+    return humanReadableShortcutNames.value(cmdName, cmdName);
 }
