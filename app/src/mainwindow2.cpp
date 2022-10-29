40a7d81e06b0143077c50e21d20ce81d1b721f72
Oliver Stevns <1045397+MrStevns@users.noreply.github.com>
2022-10-29 23:42:36 +0200

    Toolbar icon resizing (#6)
    * Revert "Adjust icon size"
    
    This reverts commit 67e113ae648d8d3bacdd6c787c524634749377a9.
    
    * Change icon size of overlay toolbar icons
    
    * Set icon size to 22 pixels

--- app/src/mainwindow2.cpp
+++ app/src/mainwindow2.cpp
@@ -1701,6 +1701,7 @@ void MainWindow2::createToolbars()
     mOverlayToolbar->addAction(ui->actionOnePointPerspective);
     mOverlayToolbar->addAction(ui->actionTwoPointPerspective);
     mOverlayToolbar->addAction(ui->actionThreePointPerspective);
+    mOverlayToolbar->setIconSize(QSize(22,22));
     QToolButton* perspectiveLinesAngleButton = new QToolButton(this);
     perspectiveLinesAngleButton->setDefaultAction(ui->menuPerspectiveLinesAngle->menuAction());
     perspectiveLinesAngleButton->setPopupMode(QToolButton::InstantPopup);
