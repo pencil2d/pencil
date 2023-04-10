6d71b502618c49e0f6c6b815b38fa02df0598eb1
Jakob <j5lx@fmail.co.uk>
2023-04-11 00:20:57 +0200

    Fix move tool axis lock and timeline update issues (#1754)
    * Fix move tool centering selection on cursor when locking axis
    
    * Fix timeline not updating when moving frame via action command
    
    * Rename SelectionManager::mAlignToPosition to mDragOrigin

--- core_lib/src/tool/movetool.h
+++ core_lib/src/tool/movetool.h
@@ -48,22 +48,18 @@ public:
 private:
     void cancelChanges();
     void applyTransformation();
-    void setAnchorToLastPoint();
     void updateSettings(const SETTING setting);
 
     void beginInteraction(Qt::KeyboardModifiers keyMod, Layer* layer);
     void createVectorSelection(Qt::KeyboardModifiers keyMod, Layer* layer);
-    void transformSelection(Qt::KeyboardModifiers keyMod, Layer* layer);
+    void transformSelection(Qt::KeyboardModifiers keyMod);
     void storeClosestVectorCurve(Layer* layer);
 
     void setCurveSelected(VectorImage* vectorImage, Qt::KeyboardModifiers keyMod);
     void setAreaSelected(VectorImage* vectorImage, Qt::KeyboardModifiers keyMod);
 
-    QPointF offsetFromPressPos();
-
     Layer* currentPaintableLayer();
 
-    QPointF anchorOriginPoint;
     Layer* mCurrentLayer = nullptr;
     qreal mRotatedAngle = 0.0;
     qreal mPreviousAngle = 0.0;
