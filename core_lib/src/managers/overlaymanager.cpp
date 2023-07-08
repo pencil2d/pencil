1b4e39005e263f82a2eb932da171f2c9ea8e3a1d
MrStevns <CandyFace18@gmail.com>
2023-07-08 14:39:10 +0200

    Fix Three point perspective can't drag left/right point

--- core_lib/src/managers/overlaymanager.cpp
+++ core_lib/src/managers/overlaymanager.cpp
@@ -80,11 +80,11 @@ MoveMode OverlayManager::getMoveModeForPoint(const QPointF& pos, const QTransfor
     {
         mode = MoveMode::PERSP_SINGLE;
     }
-    else if (mTwoPointPerspectiveEnabled && QLineF(pos, transform.inverted().map(mLeftPerspectivePoint)).length() < calculatedSelectionTol)
+    else if ((mTwoPointPerspectiveEnabled || mThreePointPerspectiveEnabled) && QLineF(pos, transform.inverted().map(mLeftPerspectivePoint)).length() < calculatedSelectionTol)
     {
         mode = MoveMode::PERSP_LEFT;
     }
-    else if (mTwoPointPerspectiveEnabled && QLineF(pos, transform.inverted().map(mRightPerspectivePoint)).length() < calculatedSelectionTol)
+    else if ((mTwoPointPerspectiveEnabled || mThreePointPerspectiveEnabled) && QLineF(pos, transform.inverted().map(mRightPerspectivePoint)).length() < calculatedSelectionTol)
     {
         mode = MoveMode::PERSP_RIGHT;
     }
