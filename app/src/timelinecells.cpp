bfe2349998bbf6cec6e977fb1a68ecb3c543493e
Pascal <81458575+khoidauminh@users.noreply.github.com>
2024-07-20 16:32:08 +0700

    Do not make a new keyframe if double clicking on an existing keyframe (#1851)
    * Do not make a new keyframe if double clicking on a keyframe
    
    * Only scrub when we create a new keyframe
    
    ---------
    
    Co-authored-by: MrStevns <CandyFace18@gmail.com>

--- app/src/timelinecells.cpp
+++ app/src/timelinecells.cpp
@@ -1130,8 +1130,11 @@ void TimeLineCells::mouseDoubleClickEvent(QMouseEvent* event)
     {
         if (mType == TIMELINE_CELL_TYPE::Tracks && (layerNumber != -1) && (frameNumber > 0) && layerNumber < mEditor->object()->getLayerCount())
         {
-            mEditor->scrubTo(frameNumber);
-            emit insertNewKeyFrame();
+            if (!layer->keyExistsWhichCovers(frameNumber))
+            {
+                mEditor->scrubTo(frameNumber);
+                emit insertNewKeyFrame();
+            }
 
             // The release event will toggle the frame on again, so we make sure it gets
             // deselected now instead.
