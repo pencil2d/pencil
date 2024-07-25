pencil2d/pencil

commit 2c5891dcfa10e274986878665c313b39ca6769a1

remove legacy Export Sound shortcut

app/src/filedialog.cpp
@@ -186,7 +186,6 @@ QString FileDialog::saveDialogCaption(FileType fileType)
     case FileType::GIF: return tr("Export Animated GIF");
     case FileType::ANIMATED_IMAGE: return tr("Export animated image");
     case FileType::MOVIE: return tr("Export movie");
-    case FileType::SOUND: return tr("Export sound");
     case FileType::PALETTE: return tr("Export palette");
     }
     return "";
@@ -218,7 +217,6 @@ QString FileDialog::saveFileFilters(FileType fileType)
     case FileType::GIF: return QString("%1 (*.gif)").arg(tr("Animated GIF"));
     case FileType::ANIMATED_IMAGE: return "";
     case FileType::MOVIE: return "MP4 (*.mp4);; AVI (*.avi);; WebM (*.webm);; APNG (*.apng)";
-    case FileType::SOUND: return "";
     case FileType::PALETTE: return PFF_PALETTE_EXT_FILTER;
     }
     return "";

app/src/shortcutspage.cpp
@@ -311,7 +311,6 @@ static QString getHumanReadableShortcutName(const QString& cmdName)
         {CMD_EXPORT_IMAGE_SEQ, ShortcutsPage::tr("Export Image Sequence", "Shortcut")},
         {CMD_EXPORT_MOVIE, ShortcutsPage::tr("Export Movie", "Shortcut")},
         {CMD_EXPORT_PALETTE, ShortcutsPage::tr("Export Palette", "Shortcut")},
-        {CMD_EXPORT_SOUND, ShortcutsPage::tr("Export Sound", "Shortcut")},
         {CMD_FLIP_HORIZONTAL, ShortcutsPage::tr("View: Horizontal Flip", "Shortcut")},
         {CMD_FLIP_VERTICAL, ShortcutsPage::tr("View: Vertical Flip", "Shortcut")},
         {CMD_FLIP_INBETWEEN, ShortcutsPage::tr("Flip In-Between", "Shortcut")},

core_lib/data/resources/kb.ini
@@ -11,7 +11,6 @@ CmdExportImageSequence=Ctrl+R
 CmdExportImage=Ctrl+Shift+R
 CmdExportMovie=
 CmdExportPalette=
-CmdExportSound=Ctrl+I
 CmdUndo=Ctrl+Z
 CmdRedo=Ctrl+Shift+Z
 CmdCut=Ctrl+X

core_lib/src/util/pencildef.h
@@ -132,7 +132,6 @@ const static int MaxFramesBound = 9999;
 #define CMD_EXPORT_IMAGE "CmdExportImage"
 #define CMD_EXPORT_MOVIE "CmdExportMovie"
 #define CMD_EXPORT_PALETTE "CmdExportPalette"
-#define CMD_EXPORT_SOUND "CmdExportSound"
 #define CMD_UNDO "CmdUndo"
 #define CMD_REDO "CmdRedo"
 #define CMD_CUT "CmdCut"

commit 8ca19c25ba999dc8a33316beaadbf2b24dcfc43c

fix warnings of missing enum arm
regression introduced by 2c5891dcfa10e274986878665c313b39ca6769a1

app/src/filedialog.cpp
@@ -186,6 +186,7 @@ QString FileDialog::saveDialogCaption(FileType fileType)
     case FileType::GIF: return tr("Export Animated GIF");
     case FileType::ANIMATED_IMAGE: return tr("Export animated image");
     case FileType::MOVIE: return tr("Export movie");
+    case FileType::SOUND: return "";
     case FileType::PALETTE: return tr("Export palette");
     }
     return "";
@@ -217,6 +218,7 @@ QString FileDialog::saveFileFilters(FileType fileType)
     case FileType::GIF: return QString("%1 (*.gif)").arg(tr("Animated GIF"));
     case FileType::ANIMATED_IMAGE: return "";
     case FileType::MOVIE: return "MP4 (*.mp4);; AVI (*.avi);; WebM (*.webm);; APNG (*.apng)";
+    case FileType::SOUND: return "";
     case FileType::PALETTE: return PFF_PALETTE_EXT_FILTER;
     }
     return "";