pencil2d/pencil

commit 2c5891dcfa10e274986878665c313b39ca6769a1

remove legacy Export Sound shortcut

app/src/filedialog.cpp
@@ -186,7 +186,6 @@ QString FileDialog::saveDialogCaption(FileType fileType)
     case FileType::GIF: return tr("Export Animated GIF");
     case FileType::ANIMATED_IMAGE: return tr("Export animated image");
     case FileType::MOVIE: return tr("Export movie");
-    case FileType::SOUND: return tr("Export sound");
     case FileType::PALETTE: return tr("Export palette");
     }
     return "";
@@ -218,7 +217,6 @@ QString FileDialog::saveFileFilters(FileType fileType)
     case FileType::GIF: return QString("%1 (*.gif)").arg(tr("Animated GIF"));
     case FileType::ANIMATED_IMAGE: return "";
     case FileType::MOVIE: return "MP4 (*.mp4);; AVI (*.avi);; WebM (*.webm);; APNG (*.apng)";
-    case FileType::SOUND: return "";
     case FileType::PALETTE: return PFF_PALETTE_EXT_FILTER;
     }
     return "";

app/src/shortcutspage.cpp
@@ -311,7 +311,6 @@ static QString getHumanReadableShortcutName(const QString& cmdName)
         {CMD_EXPORT_IMAGE_SEQ, ShortcutsPage::tr("Export Image Sequence", "Shortcut")},
         {CMD_EXPORT_MOVIE, ShortcutsPage::tr("Export Movie", "Shortcut")},
         {CMD_EXPORT_PALETTE, ShortcutsPage::tr("Export Palette", "Shortcut")},
-        {CMD_EXPORT_SOUND, ShortcutsPage::tr("Export Sound", "Shortcut")},
         {CMD_FLIP_HORIZONTAL, ShortcutsPage::tr("View: Horizontal Flip", "Shortcut")},
         {CMD_FLIP_VERTICAL, ShortcutsPage::tr("View: Vertical Flip", "Shortcut")},
         {CMD_FLIP_INBETWEEN, ShortcutsPage::tr("Flip In-Between", "Shortcut")},

core_lib/data/resources/kb.ini
@@ -11,7 +11,6 @@ CmdExportImageSequence=Ctrl+R
 CmdExportImage=Ctrl+Shift+R
 CmdExportMovie=
 CmdExportPalette=
-CmdExportSound=Ctrl+I
 CmdUndo=Ctrl+Z
 CmdRedo=Ctrl+Shift+Z
 CmdCut=Ctrl+X

core_lib/src/util/pencildef.h
@@ -132,7 +132,6 @@ const static int MaxFramesBound = 9999;
 #define CMD_EXPORT_IMAGE "CmdExportImage"
 #define CMD_EXPORT_MOVIE "CmdExportMovie"
 #define CMD_EXPORT_PALETTE "CmdExportPalette"
-#define CMD_EXPORT_SOUND "CmdExportSound"
 #define CMD_UNDO "CmdUndo"
 #define CMD_REDO "CmdRedo"
 #define CMD_CUT "CmdCut"

commit cfeeed89a1b01c512e31ecab1e49f7e0ddd4acd2

Updated shortcuts (#1866)
* remove legacy Export Sound shortcut

* add some missing keyboard shortcuts

- Export Animated GIF
- Playback range
- Peg bar Alignment
- Import Image Predefined Set
- Import Movie Video
- Import Movie Audio
- Import Animated Image
- Import Layers from project file
- Import Palette (Append)
- Reset all tools to default
- Change Line Color (Current keyframe)
- Change Line Color (All keyframes on layer)
- Toggle Center Overlay
- Toggle Thirds Overlay
- Toggle Golden Ratio Overlay
- Toggle Safe Areas Overlay
- Toggle One Point Perspective Overlay
- Toggle Two Point Perspective Overlay
- Toggle Three Point Perspective Overlay

* better shortcut name for CMD_LOOP_CONTROL

* setup shortcut for Import Palette (Replace)

* fix warnings of missing enum arm

regression introduced by 2c5891dcfa10e274986878665c313b39ca6769a1

* remove code related to incomplete Preview feature

- preview.h
- preview.cpp
- actionPreview
- CMD_PREVIEW
- CmdPreview

* add two more missing shortcuts

- Selection: Reposition Frames
- Change Layer / Keyframe Opacity

app/app.pro
@@ -88,7 +88,6 @@ HEADERS += \
     src/shortcutspage.h \
     src/timelinepage.h \
     src/toolspage.h \
-    src/preview.h \
     src/basedockwidget.h \
     src/colorbox.h \
     src/colorinspector.h \
@@ -140,7 +139,6 @@ SOURCES += \
     src/shortcutspage.cpp \
     src/timelinepage.cpp \
     src/toolspage.cpp \
-    src/preview.cpp \
     src/basedockwidget.cpp \
     src/colorbox.cpp \
     src/colorinspector.cpp \

app/src/filedialog.cpp
@@ -186,7 +186,7 @@ QString FileDialog::saveDialogCaption(FileType fileType)
     case FileType::GIF: return tr("Export Animated GIF");
     case FileType::ANIMATED_IMAGE: return tr("Export animated image");
     case FileType::MOVIE: return tr("Export movie");
-    case FileType::SOUND: return tr("Export sound");
+    case FileType::SOUND: return "";
     case FileType::PALETTE: return tr("Export palette");
     }
     return "";

app/src/mainwindow2.cpp
@@ -68,7 +68,6 @@ GNU General Public License for more details.
 #include "pegbaralignmentdialog.h"
 #include "repositionframesdialog.h"
 
-//#include "preview.h"
 #include "errordialog.h"
 #include "filedialog.h"
 #include "importimageseqdialog.h"
@@ -1165,6 +1164,7 @@ void MainWindow2::setupKeyboardShortcuts()
         return keySequence;
     };
 
+    // File menu
     ui->actionNew->setShortcut(cmdKeySeq(CMD_NEW_FILE));
     ui->actionOpen->setShortcut(cmdKeySeq(CMD_OPEN_FILE));
     ui->actionSave->setShortcut(cmdKeySeq(CMD_SAVE_FILE));
@@ -1172,14 +1172,19 @@ void MainWindow2::setupKeyboardShortcuts()
 
     ui->actionImport_Image->setShortcut(cmdKeySeq(CMD_IMPORT_IMAGE));
     ui->actionImport_ImageSeq->setShortcut(cmdKeySeq(CMD_IMPORT_IMAGE_SEQ));
+    ui->actionImport_ImageSeqNum->setShortcut(cmdKeySeq(CMD_IMPORT_IMAGE_PREDEFINED_SET));
     ui->actionImport_MovieVideo->setShortcut(cmdKeySeq(CMD_IMPORT_MOVIE_VIDEO));
+    ui->actionImport_AnimatedImage->setShortcut(cmdKeySeq(CMD_IMPORT_ANIMATED_IMAGE));
+    ui->actionImportLayers_from_pclx->setShortcut(cmdKeySeq(CMD_IMPORT_LAYERS));
+    ui->actionImport_Sound->setShortcut(cmdKeySeq(CMD_IMPORT_SOUND));
     ui->actionImport_MovieAudio->setShortcut(cmdKeySeq(CMD_IMPORT_MOVIE_AUDIO));
     ui->actionImport_Append_Palette->setShortcut(cmdKeySeq(CMD_IMPORT_PALETTE));
-    ui->actionImport_Sound->setShortcut(cmdKeySeq(CMD_IMPORT_SOUND));
+    ui->actionImport_Replace_Palette->setShortcut(cmdKeySeq(CMD_IMPORT_PALETTE_REPLACE));
 
     ui->actionExport_Image->setShortcut(cmdKeySeq(CMD_EXPORT_IMAGE));
     ui->actionExport_ImageSeq->setShortcut(cmdKeySeq(CMD_EXPORT_IMAGE_SEQ));
     ui->actionExport_Movie->setShortcut(cmdKeySeq(CMD_EXPORT_MOVIE));
+    ui->actionExport_Animated_GIF->setShortcut(cmdKeySeq(CMD_EXPORT_GIF));
     ui->actionExport_Palette->setShortcut(cmdKeySeq(CMD_EXPORT_PALETTE));
 
     // edit menu
@@ -1194,10 +1199,12 @@ void MainWindow2::setupKeyboardShortcuts()
     ui->actionFlip_Y->setShortcut(cmdKeySeq(CMD_SELECTION_FLIP_VERTICAL));
     ui->actionSelect_All->setShortcut(cmdKeySeq(CMD_SELECT_ALL));
     ui->actionDeselect_All->setShortcut(cmdKeySeq(CMD_DESELECT_ALL));
+    ui->actionPegbarAlignment->setShortcut(cmdKeySeq(CMD_PEGBAR_ALIGNMENT));
     ui->actionPreference->setShortcut(cmdKeySeq(CMD_PREFERENCE));
 
     // View menu
     ui->actionResetWindows->setShortcut(cmdKeySeq(CMD_RESET_WINDOWS));
+    ui->actionLockWindows->setShortcut(cmdKeySeq(CMD_LOCK_WINDOWS));
     ui->actionReset_View->setShortcut(cmdKeySeq(CMD_RESET_ZOOM_ROTATE));
     ui->actionCenter_View->setShortcut(cmdKeySeq(CMD_CENTER_VIEW));
     ui->actionZoom_In->setShortcut(cmdKeySeq(CMD_ZOOM_IN));
@@ -1214,14 +1221,22 @@ void MainWindow2::setupKeyboardShortcuts()
     ui->actionReset_Rotation->setShortcut(cmdKeySeq(CMD_RESET_ROTATION));
     ui->actionHorizontal_Flip->setShortcut(cmdKeySeq(CMD_FLIP_HORIZONTAL));
     ui->actionVertical_Flip->setShortcut(cmdKeySeq(CMD_FLIP_VERTICAL));
-    ui->actionPreview->setShortcut(cmdKeySeq(CMD_PREVIEW));
     ui->actionGrid->setShortcut(cmdKeySeq(CMD_GRID));
+    ui->actionCenter->setShortcut(cmdKeySeq(CMD_OVERLAY_CENTER));
+    ui->actionThirds->setShortcut(cmdKeySeq(CMD_OVERLAY_THIRDS));
+    ui->actionGoldenRatio->setShortcut(cmdKeySeq(CMD_OVERLAY_GOLDEN_RATIO));
+    ui->actionSafeAreas->setShortcut(cmdKeySeq(CMD_OVERLAY_SAFE_AREAS));
+    ui->actionOnePointPerspective->setShortcut(cmdKeySeq(CMD_OVERLAY_ONE_POINT_PERSPECTIVE));
+    ui->actionTwoPointPerspective->setShortcut(cmdKeySeq(CMD_OVERLAY_TWO_POINT_PERSPECTIVE));
+    ui->actionThreePointPerspective->setShortcut(cmdKeySeq(CMD_OVERLAY_THREE_POINT_PERSPECTIVE));
     ui->actionOnionPrev->setShortcut(cmdKeySeq(CMD_ONIONSKIN_PREV));
     ui->actionOnionNext->setShortcut(cmdKeySeq(CMD_ONIONSKIN_NEXT));
     ui->actionStatusBar->setShortcut(cmdKeySeq(CMD_TOGGLE_STATUS_BAR));
 
+    // Animation menu
     ui->actionPlay->setShortcut(cmdKeySeq(CMD_PLAY));
     ui->actionLoop->setShortcut(cmdKeySeq(CMD_LOOP));
+    ui->actionLoopControl->setShortcut(cmdKeySeq(CMD_LOOP_CONTROL));
     ui->actionPrevious_Frame->setShortcut(cmdKeySeq(CMD_GOTO_PREV_FRAME));
     ui->actionNext_Frame->setShortcut(cmdKeySeq(CMD_GOTO_NEXT_FRAME));
     ui->actionPrev_KeyFrame->setShortcut(cmdKeySeq(CMD_GOTO_PREV_KEY_FRAME));
@@ -1237,6 +1252,7 @@ void MainWindow2::setupKeyboardShortcuts()
     ui->actionMove_Frame_Forward->setShortcut(cmdKeySeq(CMD_MOVE_FRAME_FORWARD));
     ui->actionFlip_inbetween->setShortcut(cmdKeySeq(CMD_FLIP_INBETWEEN));
     ui->actionFlip_rolling->setShortcut(cmdKeySeq(CMD_FLIP_ROLLING));
+    ui->actionReposition_Selected_Frames->setShortcut(cmdKeySeq(CMD_SELECTION_REPOSITION_FRAMES));
 
     ShortcutFilter* shortcutFilter = new ShortcutFilter(ui->scribbleArea, this);
     ui->actionMove->setShortcut(cmdKeySeq(CMD_TOOL_MOVE));
@@ -1250,6 +1266,7 @@ void MainWindow2::setupKeyboardShortcuts()
     ui->actionBucket->setShortcut(cmdKeySeq(CMD_TOOL_BUCKET));
     ui->actionEyedropper->setShortcut(cmdKeySeq(CMD_TOOL_EYEDROPPER));
     ui->actionEraser->setShortcut(cmdKeySeq(CMD_TOOL_ERASER));
+    ui->actionResetToolsDefault->setShortcut(cmdKeySeq(CMD_RESET_ALL_TOOLS));
 
     ui->actionMove->installEventFilter(shortcutFilter);
     ui->actionMove->installEventFilter(shortcutFilter);
@@ -1264,11 +1281,15 @@ void MainWindow2::setupKeyboardShortcuts()
     ui->actionEyedropper->installEventFilter(shortcutFilter);
     ui->actionEraser->installEventFilter(shortcutFilter);
 
+    // Layer menu
     ui->actionNew_Bitmap_Layer->setShortcut(cmdKeySeq(CMD_NEW_BITMAP_LAYER));
     ui->actionNew_Vector_Layer->setShortcut(cmdKeySeq(CMD_NEW_VECTOR_LAYER));
     ui->actionNew_Camera_Layer->setShortcut(cmdKeySeq(CMD_NEW_CAMERA_LAYER));
     ui->actionNew_Sound_Layer->setShortcut(cmdKeySeq(CMD_NEW_SOUND_LAYER));
     ui->actionDelete_Current_Layer->setShortcut(cmdKeySeq(CMD_DELETE_CUR_LAYER));
+    ui->actionChangeLineColorCurrent_keyframe->setShortcut(cmdKeySeq(CMD_CHANGE_LINE_COLOR_KEYFRAME));
+    ui->actionChangeLineColorAll_keyframes_on_layer->setShortcut(cmdKeySeq(CMD_CHANGE_LINE_COLOR_LAYER));
+    ui->actionChangeLayerOpacity->setShortcut(cmdKeySeq(CMD_CHANGE_LAYER_OPACITY));
 
     ui->actionVisibilityCurrentLayerOnly->setShortcut(cmdKeySeq(CMD_CURRENT_LAYER_VISIBILITY));
     ui->actionVisibilityRelative->setShortcut(cmdKeySeq(CMD_RELATIVE_LAYER_VISIBILITY));

app/src/preview.cpp
@@ -1,46 +1,0 @@
-/*
-
-Pencil2D - Traditional Animation Software
-Copyright (C) 2005-2007 Patrick Corrieri & Pascal Naidon
-Copyright (C) 2012-2020 Matthew Chiawen Chang
-
-This program is free software; you can redistribute it and/or
-modify it under the terms of the GNU General Public License
-as published by the Free Software Foundation; version 2 of the License.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-*/
-
-#include "preview.h"
-#include <QPainter>
-
-
-PreviewCanvas::PreviewCanvas( QWidget* parent ) : QWidget( parent )
-{
-	setFixedSize( 200, 200 );
-}
-
-void PreviewCanvas::paintEvent( QPaintEvent* )
-{
-	QPainter painter( this );
-	if ( mBitmapImage )
-	{
-		painter.drawImage( rect( ), *( mBitmapImage->image() ) );
-	}
-	painter.end( );
-}
-
-PreviewWidget::PreviewWidget( QWidget* parent ) : QDockWidget( parent )
-{
-	mCanvas = new PreviewCanvas( this );
-	setWidget( mCanvas );
-}
-
-void PreviewWidget::updateImage()
-{
-	mCanvas->update();
-}

app/src/preview.h
@@ -1,54 +1,0 @@
-/*
-
-Pencil2D - Traditional Animation Software
-Copyright (C) 2005-2007 Patrick Corrieri & Pascal Naidon
-Copyright (C) 2012-2020 Matthew Chiawen Chang
-
-This program is free software; you can redistribute it and/or
-modify it under the terms of the GNU General Public License
-as published by the Free Software Foundation; version 2 of the License.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-*/
-
-#ifndef PREVIEW_H
-#define PREVIEW_H
-
-#include <QDockWidget>
-#include "bitmapimage.h"
-
-class PreviewCanvas : public QWidget
-{
-	Q_OBJECT
-
-public:
-	PreviewCanvas( QWidget* );
-	void setImage( BitmapImage* img ) { mBitmapImage = img; }
-
-protected:
-	void paintEvent( QPaintEvent* ) override;
-
-private:
-	BitmapImage* mBitmapImage = nullptr;
-};
-
-
-
-class PreviewWidget : public QDockWidget
-{
-    Q_OBJECT
-public:
-    PreviewWidget(QWidget *parent = nullptr);
-	void setImage( BitmapImage* img ) { mCanvas->setImage( img ); }
-	void updateImage();
-
-private:
-	PreviewCanvas* mCanvas = nullptr;
-	
-};
-
-#endif // PREVIEW_H

app/src/shortcutspage.cpp
@@ -310,8 +310,8 @@ static QString getHumanReadableShortcutName(const QString& cmdName)
         {CMD_EXPORT_IMAGE, ShortcutsPage::tr("Export Image", "Shortcut")},
         {CMD_EXPORT_IMAGE_SEQ, ShortcutsPage::tr("Export Image Sequence", "Shortcut")},
         {CMD_EXPORT_MOVIE, ShortcutsPage::tr("Export Movie", "Shortcut")},
+        {CMD_EXPORT_GIF, ShortcutsPage::tr("Export Animated GIF", "Shortcut")},
         {CMD_EXPORT_PALETTE, ShortcutsPage::tr("Export Palette", "Shortcut")},
-        {CMD_EXPORT_SOUND, ShortcutsPage::tr("Export Sound", "Shortcut")},
         {CMD_FLIP_HORIZONTAL, ShortcutsPage::tr("View: Horizontal Flip", "Shortcut")},
         {CMD_FLIP_VERTICAL, ShortcutsPage::tr("View: Vertical Flip", "Shortcut")},
         {CMD_FLIP_INBETWEEN, ShortcutsPage::tr("Flip In-Between", "Shortcut")},
@@ -322,18 +322,34 @@ static QString getHumanReadableShortcutName(const QString& cmdName)
         {CMD_SELECTION_FLIP_HORIZONTAL, ShortcutsPage::tr("Selection: Horizontal Flip", "Shortcut")},
         {CMD_SELECTION_FLIP_VERTICAL, ShortcutsPage::tr("Selection: Vertical Flip", "Shortcut")},
         {CMD_GOTO_PREV_KEY_FRAME, ShortcutsPage::tr("Previous Keyframe", "Shortcut")},
+        {CMD_SELECTION_REPOSITION_FRAMES, ShortcutsPage::tr("Selection: Reposition Frames", "Shortcut")},
         {CMD_SELECTION_ADD_FRAME_EXPOSURE, ShortcutsPage::tr("Selection: Add Frame Exposure", "Shortcut")},
         {CMD_SELECTION_SUBTRACT_FRAME_EXPOSURE, ShortcutsPage::tr("Selection: Subtract Frame Exposure", "Shortcut")},
         {CMD_REVERSE_SELECTED_FRAMES, ShortcutsPage::tr("Selection: Reverse Keyframes", "Shortcut")},
         {CMD_REMOVE_SELECTED_FRAMES, ShortcutsPage::tr("Selection: Remove Keyframes", "Shortcut")},
         {CMD_GRID, ShortcutsPage::tr("Toggle Grid", "Shortcut")},
+        {CMD_OVERLAY_CENTER, ShortcutsPage::tr("Toggle Center Overlay", "Shortcut")},
+        {CMD_OVERLAY_THIRDS, ShortcutsPage::tr("Toggle Thirds Overlay", "Shortcut")},
+        {CMD_OVERLAY_GOLDEN_RATIO, ShortcutsPage::tr("Toggle Golden Ratio Overlay", "Shortcut")},
+        {CMD_OVERLAY_SAFE_AREAS, ShortcutsPage::tr("Toggle Safe Areas Overlay", "Shortcut")},
+        {CMD_OVERLAY_ONE_POINT_PERSPECTIVE, ShortcutsPage::tr("Toggle One Point Perspective Overlay", "Shortcut")},
+        {CMD_OVERLAY_TWO_POINT_PERSPECTIVE, ShortcutsPage::tr("Toggle Two Point Perspective Overlay", "Shortcut")},
+        {CMD_OVERLAY_THREE_POINT_PERSPECTIVE, ShortcutsPage::tr("Toggle Three Point Perspective Overlay", "Shortcut")},
         {CMD_IMPORT_IMAGE, ShortcutsPage::tr("Import Image", "Shortcut")},
         {CMD_IMPORT_IMAGE_SEQ, ShortcutsPage::tr("Import Image Sequence", "Shortcut")},
+        {CMD_IMPORT_IMAGE_PREDEFINED_SET, ShortcutsPage::tr("Import Image Predefined Set", "Shortcut")},
+        {CMD_IMPORT_MOVIE_VIDEO, ShortcutsPage::tr("Import Movie Video", "Shortcut")},
+        {CMD_IMPORT_MOVIE_AUDIO, ShortcutsPage::tr("Import Movie Audio", "Shortcut")},
+        {CMD_IMPORT_ANIMATED_IMAGE, ShortcutsPage::tr("Import Animated Image", "Shortcut")},
+        {CMD_IMPORT_LAYERS, ShortcutsPage::tr("Import Layers from project file", "Shortcut")},
+        {CMD_IMPORT_PALETTE, ShortcutsPage::tr("Import Palette (Append)", "Shortcut")},
+        {CMD_IMPORT_PALETTE_REPLACE, ShortcutsPage::tr("Import Palette (Replace)", "Shortcut")},
         {CMD_IMPORT_SOUND, ShortcutsPage::tr("Import Sound", "Shortcut")},
         {CMD_ALL_LAYER_VISIBILITY, ShortcutsPage::tr("Show All Layers", "Shortcut")},
         {CMD_CURRENT_LAYER_VISIBILITY, ShortcutsPage::tr("Show Current Layer Only", "Shortcut")},
         {CMD_RELATIVE_LAYER_VISIBILITY, ShortcutsPage::tr("Show Layers Relative to Current Layer", "Shortcut")},
         {CMD_LOOP, ShortcutsPage::tr("Toggle Loop", "Shortcut")},
+        {CMD_LOOP_CONTROL, ShortcutsPage::tr("Toggle Range Playback", "Shortcut")},
         {CMD_MOVE_FRAME_BACKWARD, ShortcutsPage::tr("Move Frame Backward", "Shortcut")},
         {CMD_MOVE_FRAME_FORWARD, ShortcutsPage::tr("Move Frame Forward", "Shortcut")},
         {CMD_NEW_BITMAP_LAYER, ShortcutsPage::tr("New Bitmap Layer", "Shortcut")},
@@ -346,11 +362,12 @@ static QString getHumanReadableShortcutName(const QString& cmdName)
         {CMD_OPEN_FILE, ShortcutsPage::tr("Open File", "Shortcut")},
         {CMD_PASTE, ShortcutsPage::tr("Paste", "Shortcut")},
         {CMD_PLAY, ShortcutsPage::tr("Play/Stop", "Shortcut")},
+        {CMD_PEGBAR_ALIGNMENT, ShortcutsPage::tr("Peg bar Alignment", "Shortcut")},
         {CMD_PREFERENCE, ShortcutsPage::tr("Preferences", "Shortcut")},
-        {CMD_PREVIEW, ShortcutsPage::tr("Preview", "Shortcut")},
         {CMD_REDO, ShortcutsPage::tr("Redo", "Shortcut")},
         {CMD_REMOVE_FRAME, ShortcutsPage::tr("Remove Frame", "Shortcut")},
         {CMD_RESET_WINDOWS, ShortcutsPage::tr("Reset Windows", "Shortcut")},
+        {CMD_LOCK_WINDOWS, ShortcutsPage::tr("Lock Windows", "Shortcut")},
         {CMD_RESET_ZOOM_ROTATE, ShortcutsPage::tr("Reset View", "Shortcut")},
         {CMD_CENTER_VIEW, ShortcutsPage::tr("Center View", "Shortcut")},
         {CMD_ROTATE_ANTI_CLOCK, ShortcutsPage::tr("Rotate Anticlockwise", "Shortcut")},
@@ -378,6 +395,10 @@ static QString getHumanReadableShortcutName(const QString& cmdName)
         {CMD_TOOL_POLYLINE, ShortcutsPage::tr("Polyline Tool", "Shortcut")},
         {CMD_TOOL_SELECT, ShortcutsPage::tr("Select Tool", "Shortcut")},
         {CMD_TOOL_SMUDGE, ShortcutsPage::tr("Smudge Tool", "Shortcut")},
+        {CMD_RESET_ALL_TOOLS, ShortcutsPage::tr("Reset all tools to default", "Shortcut")},
+        {CMD_CHANGE_LINE_COLOR_KEYFRAME, ShortcutsPage::tr("Change Line Color (Current keyframe)", "Shortcut")},
+        {CMD_CHANGE_LINE_COLOR_LAYER, ShortcutsPage::tr("Change Line Color (All keyframes on layer)", "Shortcut")},
+        {CMD_CHANGE_LAYER_OPACITY, ShortcutsPage::tr("Change Layer / Keyframe Opacity", "Shortcut")},
         {CMD_UNDO, ShortcutsPage::tr("Undo", "Shortcut")},
         {CMD_ZOOM_100, ShortcutsPage::tr("Set Zoom to 100%", "Shortcut")},
         {CMD_ZOOM_200, ShortcutsPage::tr("Set Zoom to 200%", "Shortcut")},

app/ui/mainwindow2.ui
@@ -209,7 +209,6 @@
     <addaction name="actionShowInvisibleLines"/>
     <addaction name="actionShowOutlinesOnly"/>
     <addaction name="separator"/>
-    <addaction name="actionPreview"/>
    </widget>
    <widget class="QMenu" name="menuAnimation">
     <property name="title">
@@ -562,14 +561,6 @@
    </property>
    <property name="text">
     <string>Vertical Flip</string>
-   </property>
-  </action>
-  <action name="actionPreview">
-   <property name="enabled">
-    <bool>false</bool>
-   </property>
-   <property name="text">
-    <string>Preview</string>
    </property>
   </action>
   <action name="actionGrid">

core_lib/data/resources/kb.ini
@@ -6,12 +6,19 @@ CmdSaveAs=Ctrl+Shift+S
 CmdExit=Ctrl+Q
 CmdImportImage=
 CmdImportImageSequence=
+CmdImportImagePredefinedSet=
+CmdImportMovieVideo=
+CmdImportAnimatedImage=
+CmdImportLayers=
 CmdImportSound=
+CmdImportMovieAudio=
+CmdImportPalette=
+CmdImportPaletteReplace=
 CmdExportImageSequence=Ctrl+R
 CmdExportImage=Ctrl+Shift+R
 CmdExportMovie=
+CmdExportGIF=Ctrl+G
 CmdExportPalette=
-CmdExportSound=Ctrl+I
 CmdUndo=Ctrl+Z
 CmdRedo=Ctrl+Shift+Z
 CmdCut=Ctrl+X
@@ -21,8 +28,10 @@ CmdPaste=Ctrl+V
 CmdSelectAll=Ctrl+A
 CmdDeselectAll=Ctrl+D
 CmdClearFrame=
+CmdPegBarAlignment=
 CmdPreferences=
 CmdResetWindows=
+CmdLockWindows=
 CmdZoomIn=Ctrl+Up
 CmdZoomOut=Ctrl+Down
 CmdZoom400=4
@@ -39,13 +48,20 @@ CmdResetZoomRotate=Ctrl+H
 CmdCenterView=Ctrl+Shift+H
 CmdFlipHorizontal=Shift+H
 CmdFlipVertical=Shift+V
-CmdPreview=Alt+P
 CmdGrid=G
+CmdOverlayCenter=
+CmdOverlayThirds=
+CmdOverlayGoldenRatio=
+CmdOverlaySafeAreas=
+CmdOverlayOnePointPerspective=
+CmdOverlayTwoPointPerspective=
+CmdOverlayThreePointPerspective=
 CmdOnionSkinPrevious=O
 CmdOnionSkinNext=Alt+O
 CmdToggleStatusBar=
 CmdPlay=Ctrl+Return
 CmdLoop=Ctrl+L
+CmdLoopControl=
 CmdFlipInBetween=Alt+Z
 CmdFlipRolling=Alt+X
 CmdGotoNextFrame=.
@@ -60,6 +76,7 @@ CmdSelectionFlipHorizontal=
 CmdSelectionFlipVertical=
 CmdSelectionAddFrameExposure=Ctrl+"+"
 CmdSelectionSubtractFrameExposure=Ctrl+"-"
+CmdSelectionRepositionFrames=
 CmdReverseSelectedFrames=
 CmdRemoveSelectedFrames=
 CmdRemoveFrame=Shift+F5
@@ -74,6 +91,7 @@ CmdToolPencil=N
 CmdToolBucket=K
 CmdToolEyedropper=I
 CmdToolEraser=E
+CmdResetAllTools=
 CmdNewBitmapLayer=Ctrl+Alt+B
 CmdNewVectorLayer=Ctrl+Alt+V
 CmdNewSoundLayer=Ctrl+Alt+W
@@ -82,6 +100,9 @@ CmdLayerVisibilityCurrentOnly=Alt+1
 CmdLayerVisibilityRelative=Alt+2
 CmdLayerVisibilityAll=Alt+3
 CmdDeleteCurrentLayer=
+CmdChangeLineColorKeyframe=
+CmdChangeLineColorLayer=
+CmdChangeLayerOpacity=
 CmdToggleToolBox=Ctrl+1
 CmdToggleToolOptions=Ctrl+2
 CmdToggleColorWheel=Ctrl+3

core_lib/src/util/pencildef.h
@@ -125,15 +125,19 @@ const static int MaxFramesBound = 9999;
 #define CMD_SAVE_AS "CmdSaveAs"
 #define CMD_IMPORT_IMAGE "CmdImportImage"
 #define CMD_IMPORT_IMAGE_SEQ "CmdImportImageSequence"
+#define CMD_IMPORT_IMAGE_PREDEFINED_SET "CmdImportImagePredefinedSet"
 #define CMD_IMPORT_MOVIE_VIDEO "CmdImportMovieVideo"
+#define CMD_IMPORT_ANIMATED_IMAGE "CmdImportAnimatedImage"
+#define CMD_IMPORT_LAYERS "CmdImportLayers"
+#define CMD_IMPORT_SOUND "CmdImportSound"
 #define CMD_IMPORT_MOVIE_AUDIO "CmdImportMovieAudio"
 #define CMD_IMPORT_PALETTE "CmdImportPalette"
-#define CMD_IMPORT_SOUND "CmdImportSound"
+#define CMD_IMPORT_PALETTE_REPLACE "CmdImportPaletteReplace"
 #define CMD_EXPORT_IMAGE_SEQ "CmdExportImageSequence"
 #define CMD_EXPORT_IMAGE "CmdExportImage"
 #define CMD_EXPORT_MOVIE "CmdExportMovie"
+#define CMD_EXPORT_GIF "CmdExportGIF"
 #define CMD_EXPORT_PALETTE "CmdExportPalette"
-#define CMD_EXPORT_SOUND "CmdExportSound"
 #define CMD_UNDO "CmdUndo"
 #define CMD_REDO "CmdRedo"
 #define CMD_CUT "CmdCut"
@@ -143,8 +147,10 @@ const static int MaxFramesBound = 9999;
 #define CMD_SELECT_ALL "CmdSelectAll"
 #define CMD_DESELECT_ALL "CmdDeselectAll"
 #define CMD_CLEAR_FRAME "CmdClearFrame"
+#define CMD_PEGBAR_ALIGNMENT "CmdPegBarAlignment"
 #define CMD_PREFERENCE "CmdPreferences"
 #define CMD_RESET_WINDOWS "CmdResetWindows"
+#define CMD_LOCK_WINDOWS "CmdLockWindows"
 #define CMD_ZOOM_IN "CmdZoomIn"
 #define CMD_ZOOM_OUT "CmdZoomOut"
 #define CMD_ROTATE_CLOCK "CmdRotateClockwise"
@@ -161,13 +167,20 @@ const static int MaxFramesBound = 9999;
 #define CMD_ZOOM_25 "CmdZoom25"
 #define CMD_FLIP_HORIZONTAL "CmdFlipHorizontal"
 #define CMD_FLIP_VERTICAL "CmdFlipVertical"
-#define CMD_PREVIEW "CmdPreview"
 #define CMD_GRID "CmdGrid"
+#define CMD_OVERLAY_CENTER "CmdOverlayCenter"
+#define CMD_OVERLAY_THIRDS "CmdOverlayThirds"
+#define CMD_OVERLAY_GOLDEN_RATIO "CmdOverlayGoldenRatio"
+#define CMD_OVERLAY_SAFE_AREAS "CmdOverlaySafeAreas"
+#define CMD_OVERLAY_ONE_POINT_PERSPECTIVE "CmdOverlayOnePointPerspective"
+#define CMD_OVERLAY_TWO_POINT_PERSPECTIVE "CmdOverlayTwoPointPerspective"
+#define CMD_OVERLAY_THREE_POINT_PERSPECTIVE "CmdOverlayThreePointPerspective"
 #define CMD_ONIONSKIN_PREV "CmdOnionSkinPrevious"
 #define CMD_ONIONSKIN_NEXT "CmdOnionSkinNext"
 #define CMD_TOGGLE_STATUS_BAR "CmdToggleStatusBar"
 #define CMD_PLAY "CmdPlay"
 #define CMD_LOOP "CmdLoop"
+#define CMD_LOOP_CONTROL "CmdLoopControl"
 #define CMD_FLIP_INBETWEEN "CmdFlipInBetween"
 #define CMD_FLIP_ROLLING "CmdFlipRolling"
 #define CMD_GOTO_NEXT_FRAME "CmdGotoNextFrame"
@@ -179,6 +192,7 @@ const static int MaxFramesBound = 9999;
 #define CMD_REMOVE_FRAME "CmdRemoveFrame"
 #define CMD_REVERSE_SELECTED_FRAMES "CmdReverseSelectedFrames"
 #define CMD_REMOVE_SELECTED_FRAMES "CmdRemoveSelectedFrames"
+#define CMD_SELECTION_REPOSITION_FRAMES "CmdSelectionRepositionFrames"
 #define CMD_SELECTION_ADD_FRAME_EXPOSURE "CmdSelectionAddFrameExposure"
 #define CMD_SELECTION_SUBTRACT_FRAME_EXPOSURE "CmdSelectionSubtractFrameExposure"
 #define CMD_SELECTION_FLIP_HORIZONTAL "CmdSelectionFlipHorizontal"
@@ -196,11 +210,15 @@ const static int MaxFramesBound = 9999;
 #define CMD_TOOL_BUCKET "CmdToolBucket"
 #define CMD_TOOL_EYEDROPPER "CmdToolEyedropper"
 #define CMD_TOOL_ERASER "CmdToolEraser"
+#define CMD_RESET_ALL_TOOLS "CmdResetAllTools"
 #define CMD_NEW_BITMAP_LAYER "CmdNewBitmapLayer"
 #define CMD_NEW_VECTOR_LAYER "CmdNewVectorLayer"
 #define CMD_NEW_SOUND_LAYER "CmdNewSoundLayer"
 #define CMD_NEW_CAMERA_LAYER "CmdNewCameraLayer"
 #define CMD_DELETE_CUR_LAYER "CmdDeleteCurrentLayer"
+#define CMD_CHANGE_LINE_COLOR_KEYFRAME "CmdChangeLineColorKeyframe"
+#define CMD_CHANGE_LINE_COLOR_LAYER "CmdChangeLineColorLayer"
+#define CMD_CHANGE_LAYER_OPACITY "CmdChangeLayerOpacity"
 #define CMD_CURRENT_LAYER_VISIBILITY "CmdLayerVisibilityCurrentOnly"
 #define CMD_RELATIVE_LAYER_VISIBILITY "CmdLayerVisibilityRelative"
 #define CMD_ALL_LAYER_VISIBILITY "CmdLayerVisibilityAll"