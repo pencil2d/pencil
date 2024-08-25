f2f1c31875528870944721ca4d6f19602ccd8160
MrStevns <CandyFace18@gmail.com>
2024-08-25 10:16:35 +0200

    AC-1873 - Fix frame not being drawn on the timeline
    The reason for this is that while we keep a list of keyframes to move and paint keyframes on the timeline, a keyframe also has its own "isSelected" state. This state is backed up through undo/redo operations which the layer::getSelectedFrames is not.
    
    The solution is to remove isSelected from Keyframe, as there should only be one truth.

--- core_lib/src/structure/layer.cpp
+++ core_lib/src/structure/layer.cpp
@@ -222,9 +222,7 @@ bool Layer::removeKeyFrame(int position)
 
     if (frame)
     {
-        if (frame->isSelected()) {
-            removeFromSelectionList(frame->pos());
-        }
+        removeFromSelectionList(frame->pos());
         mKeyFrames.erase(frame->pos());
         markFrameAsDirty(frame->pos());
         delete frame;
@@ -383,9 +381,7 @@ bool Layer::isFrameSelected(int position) const
     KeyFrame* keyFrame = getKeyFrameWhichCovers(position);
     if (keyFrame == nullptr) { return false; }
 
-    int frameFound = mSelectedFrames_byLast.contains(keyFrame->pos());
-    Q_ASSERT(!frameFound || keyFrame->isSelected());
-    return frameFound;
+    return mSelectedFrames_byLast.contains(keyFrame->pos());
 }
 
 void Layer::setFrameSelected(int position, bool isSelected)
@@ -410,7 +406,6 @@ void Layer::setFrameSelected(int position, bool isSelected)
             mSelectedFrames_byLast.removeOne(startPosition);
             mSelectedFrames_byPosition.removeOne(startPosition);
         }
-        keyFrame->setSelected(isSelected);
     }
 }
 
@@ -496,11 +491,6 @@ void Layer::deselectAll()
 {
     mSelectedFrames_byLast.clear();
     mSelectedFrames_byPosition.clear();
-
-    for (auto pair : mKeyFrames)
-    {
-        pair.second->setSelected(false);
-    }
 }
 
 bool Layer::canMoveSelectedFramesToOffset(int offset) const
