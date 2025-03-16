697e2c28c223b9d84cd2f6df715386c39c2c8a1e
Jakob Gahde <j5lx@fmail.co.uk>
2025-03-16 22:03:52 +0100

    Fix mismatched header guard and add missing licence header

--- core_lib/src/util/importimageconfig.h
+++ core_lib/src/util/importimageconfig.h
@@ -1,5 +1,22 @@
-#ifndef IMPORTPOSITIONCONFIG_H
-#define IMPORTPOSITIONCONFIG_H
+/*
+
+Pencil2D - Traditional Animation Software
+Copyright (C) 2005-2007 Patrick Corrieri & Pascal Naidon
+Copyright (C) 2012-2020 Matthew Chiawen Chang
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; version 2 of the License.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+*/
+
+#ifndef IMPORTIMAGECONFIG_H
+#define IMPORTIMAGECONFIG_H
 
 struct ImportImageConfig
 {
