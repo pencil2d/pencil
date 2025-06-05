2db7982954d42cc5f7572f2835413eda7816c135
Oliver Stevns <1045397+MrStevns@users.noreply.github.com>
2025-06-05 09:57:07 +0200

    Fix #1873 - Fix frame not being drawn on the timeline (#1887)
    * AC-1873 - Fix frame not being drawn on the timeline
    
    The reason for this is that while we keep a list of keyframes to move and paint keyframes on the timeline, a keyframe also has its own "isSelected" state. This state is backed up through undo/redo operations which the layer::getSelectedFrames is not.
    
    The solution is to remove isSelected from Keyframe, as there should only be one truth.
    
    * Only deselect layer if the index has changed
    
    * TimeLineCells: Use ForEach and return
    
    * Cleanup unused function

--- app/src/timelinecells.cpp
+++ app/src/timelinecells.cpp
@@ -445,11 +445,17 @@ void TimeLineCells::paintFrames(QPainter& painter, QColor trackCol, const Layer*
 
     int recHeight = height - 4;
 
+    const QList<int> selectedFrames = layer->getSelectedFramesByPos();
     layer->foreachKeyFrame([&](KeyFrame* key)
     {
         int framePos = key->pos();
         int recWidth = standardWidth;
         int recLeft = getFrameX(framePos) - recWidth;
+
+        // Selected frames are painted separately
+        if (selectedFrames.contains(framePos)) {
+            return;
+        }
 
         if (key->length() > 1)
         {
@@ -464,18 +470,10 @@ void TimeLineCells::paintFrames(QPainter& painter, QColor trackCol, const Layer*
         // Paint the frame contents
         if (selected)
         {
-            if (key->isSelected()) {
-                painter.setBrush(QColor(60, 60, 60));
-            }
-            else
-            {
-                painter.setBrush(QColor(trackCol.red(), trackCol.green(), trackCol.blue(), 150));
-            }
+            painter.setBrush(QColor(trackCol.red(), trackCol.green(), trackCol.blue(), 150));
         }
 
-        if (!key->isSelected()) {
-            painter.drawRect(recLeft, recTop, recWidth, recHeight);
-        }
+        painter.drawRect(recLeft, recTop, recWidth, recHeight);
     });
 }
 
@@ -730,7 +728,7 @@ void TimeLineCells::paintEvent(QPaintEvent*)
         int currentFrame = mEditor->currentFrame();
         Layer* currentLayer = mEditor->layers()->currentLayer();
         KeyFrame* keyFrame = currentLayer->getKeyFrameWhichCovers(currentFrame);
-        if (keyFrame != nullptr && !keyFrame->isSelected())
+        if (keyFrame != nullptr)
         {
             int recWidth = keyFrame->length() == 1 ? mFrameSize - 2 : mFrameSize * keyFrame->length();
             int recLeft = getFrameX(keyFrame->pos()) - (mFrameSize - 2);
