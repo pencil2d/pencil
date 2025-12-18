6de5c1e33abf77e5ec7c7e3e74af7b1cf7b0c1db
MrStevns <CandyFace18@gmail.com>
2025-12-18 18:52:49 +0100

    Iss-1971: Fix selection bound increasing due to precision loss
    The fix is to simply truncate the value

--- core_lib/src/tool/selecttool.h
+++ core_lib/src/tool/selecttool.h
@@ -21,6 +21,8 @@ GNU General Public License for more details.
 #include "basetool.h"
 #include "movemode.h"
 
+#include "layer.h"
+
 #include <QRectF>
 
 class Layer;
@@ -47,8 +49,8 @@ private:
 
     bool keyPressEvent(QKeyEvent* event) override;
 
-    void manageSelectionOrigin(QPointF currentPoint, QPointF originPoint);
-    void controlOffsetOrigin(QPointF currentPoint, QPointF anchorPoint);
+    void manageSelectionOrigin(QPointF currentPoint, QPointF originPoint, Layer::LAYER_TYPE layerType);
+    void controlOffsetOrigin(QPointF currentPoint, QPointF anchorPoint, Layer::LAYER_TYPE layerType);
 
     void beginSelection(Layer* currentLayer, const QPointF& pos);
     void keepSelection(Layer* currentLayer);
