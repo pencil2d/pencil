d120a5d328e7a21efc4c717e76e303a9ff77e66f
scribblemaniac <scribblemaniac@users.noreply.github.com>
2025-12-24 16:35:53 -0700

    Refactor out SelectTool::manageSelectionOrigin

--- core_lib/src/tool/selecttool.h
+++ core_lib/src/tool/selecttool.h
@@ -49,7 +49,6 @@ private:
 
     bool keyPressEvent(QKeyEvent* event) override;
 
-    void manageSelectionOrigin(QPointF currentPoint, QPointF originPoint, Layer::LAYER_TYPE layerType);
     void controlOffsetOrigin(QPointF currentPoint, QPointF anchorPoint, Layer::LAYER_TYPE layerType);
 
     void beginSelection(Layer* currentLayer, const QPointF& pos);
