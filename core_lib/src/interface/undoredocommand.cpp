a7b994e9ee2ece2c5a02c16a0ca78c62cc63640f
MrStevns <CandyFace18@gmail.com>
2026-01-01 20:26:39 +0100

    fix undo/redo being ignored if frame modification was on an empty frame
    The fix is to use getLastXAtFrame rather than getFrame...

--- core_lib/src/interface/undoredocommand.cpp
+++ core_lib/src/interface/undoredocommand.cpp
@@ -218,7 +218,7 @@ BitmapReplaceCommand::BitmapReplaceCommand(const BitmapImage* undoBitmap,
     Layer* layer = editor->layers()->currentLayer();
     redoLayerId = layer->id();
     redoBitmap = *static_cast<LayerBitmap*>(layer)->
-            getBitmapImageAtFrame(editor->currentFrame());
+            getLastBitmapImageAtFrame(editor->currentFrame());
 
     setText(description);
 }
@@ -266,7 +266,7 @@ VectorReplaceCommand::VectorReplaceCommand(const VectorImage* undoVector,
     Layer* layer = editor->layers()->currentLayer();
     redoLayerId = layer->id();
     redoVector = *static_cast<LayerVector*>(layer)->
-            getVectorImageAtFrame(editor->currentFrame());
+            getLastVectorImageAtFrame(editor->currentFrame());
 
     setText(description);
 }
--- core_lib/src/managers/undoredomanager.cpp
+++ core_lib/src/managers/undoredomanager.cpp
@@ -296,9 +296,10 @@ void UndoRedoManager::initCommonKeyFrameState(UndoSaveState* undoSaveState) cons
     }
 
     const int frameIndex = editor()->currentFrame();
-    if (layer->keyExists(frameIndex))
+    KeyFrame* frame = layer->getLastKeyFrameAtPosition(frameIndex);
+    if (frame)
     {
-        undoSaveState->keyframe = std::unique_ptr<KeyFrame>(layer->getLastKeyFrameAtPosition(frameIndex)->clone());
+        undoSaveState->keyframe = std::unique_ptr<KeyFrame>(frame->clone());
     }
     else if (layer->getKeyFrameWhichCovers(frameIndex) != nullptr)
     {
