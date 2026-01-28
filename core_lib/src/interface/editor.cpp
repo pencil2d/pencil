aab41de9d37e6652cd0cc80ad8a8c992e760ce67
Matt Chang <chchwy@gmail.com>
2026-01-28 19:32:33 +0800

    Fix Qt6 compiler error

--- core_lib/src/interface/editor.cpp
+++ core_lib/src/interface/editor.cpp
@@ -815,7 +815,7 @@ Status Editor::openObject(const QString& strFilePath, const std::function<void(i
     }
     if (!fileInfo.isReadable())
     {
-        dd << QString("Permissions: 0x%1").arg(fileInfo.permissions(), 0, 16);
+        dd << QString("Permissions: 0x%1").arg(static_cast<uint>(fileInfo.permissions()), 0, 16);
         return Status(Status::ERROR_FILE_CANNOT_OPEN,
                       dd,
                       tr("Could not open file"),
