27a40ba141ff1bd4477df0cfb5f8f6f83f16426a
assemberist <schur.alexandr2015@yandex.ru>
2026-04-01 12:44:16 +0200

    Issue 1970: Implement Project Auto-Save
    Added 2 Settings to preferences->files
    1. autosaving by time
    2. autosave timer (min)
    
    Defects:
    Autosaving can break user action. Need implement check if user do some input before autosave.

--- app/src/mainwindow2.cpp
+++ app/src/mainwindow2.cpp
@@ -82,6 +82,7 @@ GNU General Public License for more details.
 #include "app_util.h"
 #include "presetdialog.h"
 #include "pegbaralignmentdialog.h"
+#include "autosaverbytime.h"
 
 
 #ifdef GIT_TIMESTAMP
@@ -135,6 +136,9 @@ MainWindow2::MainWindow2(QWidget* parent) :
     currentLayerChanged();
 
     connect(mEditor, &Editor::needSave, this, &MainWindow2::autoSave);
+
+    mAutoSaver = new autosaverbytime(mEditor->preference());
+    connect(mAutoSaver->autoSaveTimer, &QTimer::timeout, this, &MainWindow2::autoSave);
 
     mEditor->tools()->setDefaultTool();
     ui->background->init(mEditor->preference());
@@ -703,7 +707,7 @@ void MainWindow2::openStartupFile(const QString& filename)
 
     if (!filename.isEmpty() && openObject(filename))
     {
-        return;
+            return;
     }
 
     loadMostRecent() || tryLoadPreset();
@@ -1602,11 +1606,6 @@ bool MainWindow2::checkForRecoverableProjects()
 {
     FileManager fm;
     QStringList recoverables = fm.searchForUnsavedProjects();
-
-    if (recoverables.empty())
-    {
-        return false;
-    }
 
     foreach (const QString path, recoverables)
     {
